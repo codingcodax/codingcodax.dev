4fb8f425ca03c9684d692cdfff9311976ab66caa
Alexis Guzman <alexis12989x@gmail.com>
2021-11-29 11:17:16 -0600

    feat(document): edit get css text import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { getCssText } from '../stitches.config';
+import { getCssText } from '../theme/theme.config';
 
 export default class Document extends NextDocument {
   render() {
