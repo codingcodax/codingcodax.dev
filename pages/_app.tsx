49227305363a5d20ed98ba428219d5848c202d35
Alexis Guzman <alexis12989x@gmail.com>
2021-11-29 11:16:22 -0600

    feat(app): import global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app';
 
+import globalStyles from '@/theme/globalStyles';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  globalStyles();
+
   return <Component {...pageProps} />;
 };
 export default MyApp;
