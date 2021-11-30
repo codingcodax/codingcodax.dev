91a546d28407b2f6c9abce293f83a4e77d40c4dd
Alexis Guzman <alexis12989x@gmail.com>
2021-11-30 15:16:52 -0600

    feat(theme): add heading styles

--- theme/globalStyles.ts
+++ theme/globalStyles.ts
@@ -6,13 +6,41 @@ const globalStyles = globalCss({
   ...reset,
   '*, *:before, *:after': {
     boxSizing: 'border-box',
+    outline: 'none',
+  },
+
+  'button:focus': {
+    boxShadow: '$focus',
   },
 
   body: {
     fontFamily: '$body',
-    color: '$defaultLight',
-    backgroundColor: '$defaultDark',
+    fontSize: '$base',
+    color: '$textColor',
+    backgroundColor: '$backgroundColor',
   },
+
+  '#__next': {
+    padding: '$2 $4',
+    margin: '0 auto',
+    minHeight: '100vh',
+    maxWidth: '1200px',
+    display: 'grid',
+    gridTemplate: 'auto 1fr auto / 1fr',
+
+    '@media screen and (min-width: 30rem)': {
+      padding: '$8',
+    },
+  },
+
+  'h1, h2, h3, h4, h5, h6': { fontWeight: '$bold' },
+
+  h1: { fontSize: '$5xl' },
+  h2: { fontSize: '$4xl' },
+  h3: { fontSize: '$3xl' },
+  h4: { fontSize: '$2xl' },
+  h5: { fontSize: '$xl' },
+  h6: { fontSize: '$lg' },
 });
 
 export default globalStyles;
