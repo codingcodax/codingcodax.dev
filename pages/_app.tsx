5be3eaf43f529638d1930ba23adfb21a5c0ea638
Alexis Guzman <alexis12989x@gmail.com>
2021-12-01 14:06:47 -0600

    feat(app): import theme provider and layout components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 import globalStyles from '@/theme/globalStyles';
+
+import Layout from '@/components/layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   globalStyles();
 
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider defaultTheme='system'>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 };
 export default MyApp;
