50d38a700c1b8d620bdc815281f51c433f42aaa5
Alexis Guzman <alexis12989x@gmail.com>
2021-12-01 14:02:42 -0600

    feat(ui): import divider component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,3 +2,4 @@ export { default as Heading } from './Heading';
 export { default as Text } from './Text';
 export { default as Box } from './Box';
 export { default as Button } from './Button';
+export { default as Divider } from './Divider';
