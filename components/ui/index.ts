7f04e180deefb5ed6c999d9937609db025f92216
Alexis Guzman <alexis12989x@gmail.com>
2021-12-03 09:45:44 -0600

    feat(ui): import svg component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,4 +2,5 @@ export { default as Heading } from './Heading';
 export { default as Text } from './Text';
 export { default as Box } from './Box';
 export { default as Button } from './Button';
+export { default as Svg } from './Svg';
 export { default as Divider } from './Divider';
