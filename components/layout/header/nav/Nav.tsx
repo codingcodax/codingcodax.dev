8bd07a3095df75230d61f75363f33d464882d15a
Alexis Guzman <alexis12989x@gmail.com>
2021-12-03 09:39:53 -0600

    feat(header): convert ul tag to stitches component and add styles

--- components/layout/header/nav/Nav.tsx
+++ components/layout/header/nav/Nav.tsx
@@ -1,17 +1,37 @@
+import { styled } from '@/theme/theme.config';
+
 import { Box } from '@/components/ui';
 
 import NavItem from './NavItem';
+import ToggleTheme from '../ToggleTheme';
+
+const Ul = styled('ul');
 
 const Nav = () => {
   return (
     <Box
       as='nav'
-      css={{ display: 'grid', gridAutoFlow: 'column', gridColumnGap: '$4' }}
+      css={{
+        display: 'grid',
+        gridAutoFlow: 'column',
+      }}
     >
-      <Box as='ul'>
+      <Ul
+        css={{
+          mr: '$8',
+          display: 'grid',
+          gridAutoFlow: 'column',
+          alignItems: 'center',
+          gridColumnGap: '$6',
+        }}
+      >
         <NavItem href='/'>Home</NavItem>
         <NavItem href='/about'>About</NavItem>
-      </Box>
+        <NavItem href='/projects'>Projects</NavItem>
+        <NavItem href='/blog'>Blog</NavItem>
+      </Ul>
+
+      <ToggleTheme />
     </Box>
   );
 };
