e0f24c91d07ba370a8c4afa24567708374a300ed
Alexis Guzman <alexis12989x@gmail.com>
2021-12-03 09:40:39 -0600

    feat(header): create and setup toggle theme component

---
+++ components/layout/header/ToggleTheme.tsx
@@ -0,0 +1,45 @@
+import { useTheme } from 'next-themes';
+
+import { MoonIcon, SunIcon } from '@/components/icons';
+import { Button } from '@/components/ui';
+
+// const Button = styled('button');
+
+const ToggleTheme = () => {
+  const { theme, resolvedTheme, setTheme } = useTheme();
+
+  console.log(theme, resolvedTheme);
+
+  const toggleTheme = () =>
+    theme === 'dark' || resolvedTheme === 'dark '
+      ? setTheme('light')
+      : setTheme('dark');
+
+  return (
+    <Button
+      variant='unstyled'
+      onClick={toggleTheme}
+      css={{
+        p: '$0',
+        h: '$10',
+        w: '$10',
+        display: 'grid',
+        placeItems: 'center',
+        bg: '$grayDark',
+        borderRadius: '$sm',
+
+        '&:hover': {
+          bg: '$grayLight',
+        },
+      }}
+    >
+      {theme === 'light' || resolvedTheme === 'light' ? (
+        <MoonIcon css={{ h: '$4' }} />
+      ) : (
+        <SunIcon css={{ h: '$4' }} stroke='var(--colors-textColor)' />
+      )}
+    </Button>
+  );
+};
+
+export default ToggleTheme;
