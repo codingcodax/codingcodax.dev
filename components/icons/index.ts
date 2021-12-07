cd5238b5b7948c3e8843009e2bd85b2ef65a75d6
Alexis Guzman <alexis12989x@gmail.com>
2021-12-07 13:49:48 -0600

    feat(icons): import all the icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,25 @@
-export { default as Logo } from './logo';
-export { default as SunIcon } from './sun';
+export { default as AlanAIIcon } from './alanai';
+export { default as ChakraUIIcon } from './chakraui';
+export { default as CSSIcon } from './css';
+export { default as FigmaIcon } from './figma';
+export { default as FirebaseIcon } from './firebase';
+export { default as GitHubIcon } from './github';
+export { default as HtmlIcon } from './html';
+export { default as JavaScriptIcon } from './javascript';
+export { default as LinkedInIcon } from './linkedin';
+export { default as LitreekIcon } from './litreek';
+export { default as MDXIcon } from './mdx';
 export { default as MoonIcon } from './moon';
+export { default as NewspaperIcon } from './newspaper';
+export { default as NextJSIcon } from './nextjs';
+export { default as NodeJSIcon } from './nodejs';
+export { default as ReactJSIcon } from './reactjs';
+export { default as ReexJSIcon } from './reexjs';
+export { default as SASSIcon } from './sass';
+export { default as StorybookIcon } from './storybook';
+export { default as SunIcon } from './sun';
+export { default as TwitterIcon } from './twitter';
+export { default as TypeScriptIcon } from './typescript';
+export { default as YouTubeIcon } from './youtube';
+
+export { default as Logo } from './logo';
