138783c85004528807f61d808b079c77f78aa317
Alexis Guzman <alexis12989x@gmail.com>
2021-12-07 13:50:58 -0600

    feat(types): create project type

---
+++ types/data/project.ts
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+import { Tool } from 'types';
+
+type Project = {
+  logo: ReactNode;
+  title: string;
+  description: string;
+  tools: Tool[];
+  live?: string;
+  site?: string;
+  repo: string;
+  featured: boolean;
+};
+
+export default Project;
