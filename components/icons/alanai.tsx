97e0645c5e2b7d5aacb17e774e985115ce9f795d
Alexis Guzman <alexis12989x@gmail.com>
2021-12-07 13:08:26 -0600

    feat(icons): create and setup alan ai icon

---
+++ components/icons/alanai.tsx
@@ -0,0 +1,69 @@
+import { Svg } from '../ui';
+
+const AlanAi = ({ ...props }) => {
+  return (
+    <Svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='currentColor'
+      stopColor='currentColor'
+      viewBox='0 0 51 34'
+      {...props}
+    >
+      <defs />
+      <path
+        fillOpacity='.6'
+        d='M19.41 21.86h4.66c.84 0 1.6.46 1.98 1.2l4.8 9.3c.09.15.13.32.13.5 0 .6-.5 1.1-1.11 1.1h-18.1c.83 0 1.6-.47 1.98-1.2l5.66-10.9z'
+      />
+      <path d='M19.41 21.86l-5.66 10.9a2.23 2.23 0 01-1.98 1.2H.95a.84.84 0 01-.38-.1.82.82 0 01-.37-1.1l4.94-9.56a2.5 2.5 0 012.23-1.34H19.4z' />
+      <path
+        fillOpacity='.6'
+        d='M19.41 21.86H7.37c-.94 0-1.8.52-2.23 1.34L16.46 1.34A2.5 2.5 0 0118.68 0h14.87-1.08c-1.04 0-2 .58-2.47 1.5L19.4 21.85z'
+      />
+      <path d='M20.79 1.5A2.78 2.78 0 0018.3 0h-1.08H32.1c.94 0 1.8.52 2.23 1.34l16.53 31.81a.55.55 0 01-.5.8H39.03c-.84 0-1.6-.46-1.98-1.2L20.79 1.5z' />
+      <defs>
+        <linearGradient
+          x1='24.04'
+          x2='21.23'
+          y1='25.51'
+          y2='31.59'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop />
+          <stop offset='1' />
+        </linearGradient>
+        <linearGradient
+          x1='6.01'
+          x2='8.55'
+          y1='30.28'
+          y2='24.38'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop />
+          <stop offset='1' />
+        </linearGradient>
+        <linearGradient
+          x1='12.27'
+          x2='20.24'
+          y1='19.34'
+          y2='2.46'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop />
+          <stop offset='1' />
+        </linearGradient>
+        <linearGradient
+          x1='26.84'
+          x2='44.4'
+          y1='0'
+          y2='33.84'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop />
+          <stop offset='1' />
+        </linearGradient>
+      </defs>
+    </Svg>
+  );
+};
+
+export default AlanAi;
