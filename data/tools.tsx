0026e781796134c2d1951945fb83a2e5c6bcf1a0
Alexis Guzman <alexis12989x@gmail.com>
2021-12-07 13:52:46 -0600

    feat(data): create tools objects

---
+++ data/tools.tsx
@@ -0,0 +1,88 @@
+import {
+  HtmlIcon,
+  CSSIcon,
+  SASSIcon,
+  JavaScriptIcon,
+  TypeScriptIcon,
+  ReactJSIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  ChakraUIIcon,
+  FirebaseIcon,
+  StorybookIcon,
+  MDXIcon,
+  AlanAIIcon,
+  FigmaIcon,
+} from '@/components/icons';
+
+import { Tool } from 'types';
+
+export const html: Tool = {
+  name: 'HTML5',
+  icon: <HtmlIcon css={{ h: '$6' }} />,
+};
+
+export const css: Tool = {
+  name: 'CSS3',
+  icon: <CSSIcon css={{ h: '$6' }} />,
+};
+
+export const sass: Tool = {
+  name: 'SASS',
+  icon: <SASSIcon css={{ h: '$6' }} />,
+};
+
+export const javascript: Tool = {
+  name: 'JavaScript',
+  icon: <JavaScriptIcon css={{ h: '$6' }} />,
+};
+
+export const typescript: Tool = {
+  name: 'TypeScript',
+  icon: <TypeScriptIcon css={{ h: '$6' }} />,
+};
+
+export const reactjs: Tool = {
+  name: 'React JS',
+  icon: <ReactJSIcon css={{ h: '$6' }} />,
+};
+
+export const nextjs: Tool = {
+  name: 'Next JS',
+  icon: <NextJSIcon css={{ h: '$6' }} />,
+};
+
+export const nodejs: Tool = {
+  name: 'Node JS',
+  icon: <NodeJSIcon css={{ h: '$6' }} />,
+};
+
+export const chakraui: Tool = {
+  name: 'Chakra UI',
+  icon: <ChakraUIIcon css={{ h: '$6' }} />,
+};
+
+export const firebase: Tool = {
+  name: 'Firebase',
+  icon: <FirebaseIcon css={{ h: '$6' }} />,
+};
+
+export const storybook: Tool = {
+  name: 'Storybook',
+  icon: <StorybookIcon css={{ h: '$6' }} />,
+};
+
+export const mdx: Tool = {
+  name: 'MDX',
+  icon: <MDXIcon css={{ h: '$6' }} />,
+};
+
+export const alanai: Tool = {
+  name: 'MDX',
+  icon: <AlanAIIcon css={{ h: '$6' }} />,
+};
+
+export const figma: Tool = {
+  name: 'Figma',
+  icon: <FigmaIcon css={{ h: '$6' }} />,
+};
