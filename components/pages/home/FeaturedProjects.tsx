554f1ca881d320df91f1bac75a9cebceba40c9a4
Alexis Guzman <alexis12989x@gmail.com>
2021-12-08 17:34:39 -0600

    feat(home): setup featured projects component

--- components/pages/home/FeaturedProjects.tsx
+++ components/pages/home/FeaturedProjects.tsx
@@ -1,8 +1,30 @@
+import NextLink from 'next/link';
+
+import { Box, Button, Heading } from '@/components/ui';
+import Projects from '@/components/common/projects';
+
 const FeaturedProjects = () => {
   return (
-    <div>
-      <p>Featured Projects Component</p>
-    </div>
+    <Box
+      as='section'
+      css={{ display: 'grid', placeItems: 'center', gridRowGap: '$16' }}
+    >
+      <Heading
+        as='h2'
+        size={{ '@initial': '3xl', '@bp1': '4xl' }}
+        css={{ textAlign: 'center' }}
+      >
+        Featured Projects
+      </Heading>
+
+      <Projects featured />
+
+      <NextLink href='/projects' passHref>
+        <Button as='a' variant='primary' css={{ textDecoration: 'none' }}>
+          See All Projects
+        </Button>
+      </NextLink>
+    </Box>
   );
 };
 
