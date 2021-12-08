38a66866e1b491c278c2428bac211acf3bdb73d1
Alexis Guzman <alexis12989x@gmail.com>
2021-12-08 17:36:38 -0600

    feat(pages): import featured projects component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 
 import Hero from '@/components/pages/home/hero';
+import FeaturedProjects from '@/components/pages/home/FeaturedProjects';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Hero />
+      <FeaturedProjects />
     </div>
   );
 };
