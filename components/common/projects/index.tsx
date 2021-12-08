25378d753a409747197e422de39b2068e36a5c5c
Alexis Guzman <alexis12989x@gmail.com>
2021-12-08 17:34:05 -0600

    feat(common): setup projects component

--- components/common/projects/index.tsx
+++ components/common/projects/index.tsx
@@ -1,8 +1,29 @@
-const Projects = () => {
+import projects from '@/data/projects';
+
+import { Box } from '@/components/ui';
+
+import ProjectCard from './ProjectCard';
+
+const Projects = ({ featured = false }) => {
+  const projectsArray = featured
+    ? projects.filter(({ featured }) => featured)
+    : projects;
+
   return (
-    <div>
-      <p>Projects Component</p>
-    </div>
+    <Box
+      as='ul'
+      css={{
+        maxWidth: '1000px',
+        display: 'grid',
+        gridTemplate: 'auto / 1fr',
+        gridGap: '$8',
+        '@bp2': { gridTemplate: 'auto / 1fr 1fr' },
+      }}
+    >
+      {projectsArray.map((projectItem) => (
+        <ProjectCard key={projectItem.title} {...projectItem} />
+      ))}
+    </Box>
   );
 };
 
