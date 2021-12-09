fce543828e1d6205bcb94bcc92a94ae829199b2a
Alexis Guzman <alexis12989x@gmail.com>
2021-12-09 10:21:55 -0600

    feat(pages): import newsletter component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from 'next';
 
 import Hero from '@/components/pages/home/hero';
 import FeaturedProjects from '@/components/pages/home/FeaturedProjects';
+import Newsletter from '@/components/pages/home/Newsletter';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Hero />
       <FeaturedProjects />
+      <Newsletter />
     </div>
   );
 };
