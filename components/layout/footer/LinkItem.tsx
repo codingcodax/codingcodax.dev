f702ef865d6b801799a6baf4c262784559c87bd1
Alexis Guzman <alexis12989x@gmail.com>
2021-12-11 13:41:06 -0600

    feat(footer): setup link item component

--- components/layout/footer/LinkItem.tsx
+++ components/layout/footer/LinkItem.tsx
@@ -1,8 +1,32 @@
-const LinkItem = () => {
+import { FC, ReactNode } from 'react';
+
+import { styled } from '@/theme/theme.config';
+
+import Link from '@/components/common/Link';
+
+const A = styled('a');
+
+interface LinkItemProps {
+  href: string;
+  children: ReactNode;
+}
+
+const LinkItem: FC<LinkItemProps> = ({ href, children }) => {
   return (
-    <div>
-      <p>Link Item Component</p>
-    </div>
+    <li>
+      <Link href={href}>
+        <A
+          css={{
+            textDecoration: 'none',
+            color: '$grayTextColor',
+            transition: 'color $normal ease-in-out',
+            '&:hover': { color: '$grayDarkTextColor' },
+          }}
+        >
+          {children}
+        </A>
+      </Link>
+    </li>
   );
 };
 
