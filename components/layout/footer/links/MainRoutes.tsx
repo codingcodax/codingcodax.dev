1109c493a1f4da66aca8feabf0c7454493857e18
Alexis Guzman <alexis12989x@gmail.com>
2021-12-11 13:42:12 -0600

    feat(footer): setup main routes component

--- components/layout/footer/links/MainRoutes.tsx
+++ components/layout/footer/links/MainRoutes.tsx
@@ -1,8 +1,15 @@
+import { Grid } from '@/components/ui';
+
+import LinkItem from '../LinkItem';
+
 const MainRoutes = () => {
   return (
-    <div>
-      <p>Main Routes Component</p>
-    </div>
+    <Grid as='ul' css={{ gridRowGap: '$2' }}>
+      <LinkItem href='/'>Home</LinkItem>
+      <LinkItem href='/about'>About</LinkItem>
+      <LinkItem href='/projects'>Projects</LinkItem>
+      <LinkItem href='/blog'>Blog</LinkItem>
+    </Grid>
   );
 };
 
