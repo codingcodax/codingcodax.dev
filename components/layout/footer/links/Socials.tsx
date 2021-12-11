def52c946cbc5c2eae78718a572177fe017e760a
Alexis Guzman <alexis12989x@gmail.com>
2021-12-11 13:52:27 -0600

    feat(footer): setup socials component

--- components/layout/footer/links/Socials.tsx
+++ components/layout/footer/links/Socials.tsx
@@ -1,13 +1,17 @@
 import { Grid } from '@/components/ui';
 
+import SocialLinks from '@/data/socials';
+
 import LinkItem from '../LinkItem';
 
 const Socials = () => {
   return (
     <Grid as='ul' css={{ textAlign: 'right', gridRowGap: '$2' }}>
-      <LinkItem href='/github'>GitHub</LinkItem>
-      <LinkItem href='/github'>Twitter</LinkItem>
-      <LinkItem href='/github'>LinkedIn</LinkItem>
+      {SocialLinks.map(({ name, href }) => (
+        <LinkItem key={name} href={href}>
+          {name}
+        </LinkItem>
+      ))}
     </Grid>
   );
 };
