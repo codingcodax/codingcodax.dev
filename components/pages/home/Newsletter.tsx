f1e0d8895d27fb3d40e687dd024481b017bc745a
Alexis Guzman <alexis12989x@gmail.com>
2021-12-11 10:04:37 -0600

    feat(home): setup newsletter component

--- components/pages/home/Newsletter.tsx
+++ components/pages/home/Newsletter.tsx
@@ -1,8 +1,20 @@
+import { Box, Heading, Text } from '@/components/ui';
+
+import Subscribe from '@/components/common/Subscribe';
+
 const Newsletter = () => {
   return (
-    <div>
-      <p>Newsletter Component</p>
-    </div>
+    <Box as='section' align='center' css={{ my: '$32', maxWidth: '720px' }}>
+      <Heading as='h1'>Newsletter</Heading>
+      <Text css={{ mb: '$2' }}>
+        Subscribe to my digest. I{"'"}ll let you know when I publish new
+        content, and I{"'"}ll even share exclusive newsletter-only content now
+        and then.
+      </Text>
+      <Text css={{ mb: '$8' }}>No spam, unsubscribe at any time.</Text>
+
+      <Subscribe />
+    </Box>
   );
 };
 
