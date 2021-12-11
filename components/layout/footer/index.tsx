b37af4eebb2e4c961be57299be1a43e75e88bccb
Alexis Guzman <alexis12989x@gmail.com>
2021-12-11 11:03:12 -0600

    feat(footer): create links component

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -1,13 +1,17 @@
-// import useMediaQuery from '@/hooks/useMediaQuery';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 import { Box, Divider, Text } from '@/components/ui';
+import MobileLinks from './MobileLinks';
+import Links from './links';
 
 const Footer = () => {
-  // const isMobileSize = useMediaQuery('(max-width: 479px)');
+  const isMobileSize = useMediaQuery('(max-width: 479px)');
 
   return (
     <Box css={{ display: 'grid', gridRowGap: '$8' }}>
       <Divider />
+
+      {isMobileSize ? <MobileLinks /> : <Links />}
 
       <Text css={{ textAlign: 'center' }}>Copyright © 2021 Alexis Guzman</Text>
     </Box>
