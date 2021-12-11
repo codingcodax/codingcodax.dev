aac3a75acfc59a267b64ce6b293fa3773cda42dd
Alexis Guzman <alexis12989x@gmail.com>
2021-12-11 13:50:24 -0600

    feat(data): create socials object

--- components/layout/footer/links/Socials.tsx
+++ components/layout/footer/links/Socials.tsx
@@ -1,8 +1,14 @@
+import { Grid } from '@/components/ui';
+
+import LinkItem from '../LinkItem';
+
 const Socials = () => {
   return (
-    <div>
-      <p>Socials Component</p>
-    </div>
+    <Grid as='ul' css={{ textAlign: 'right', gridRowGap: '$2' }}>
+      <LinkItem href='/github'>GitHub</LinkItem>
+      <LinkItem href='/github'>Twitter</LinkItem>
+      <LinkItem href='/github'>LinkedIn</LinkItem>
+    </Grid>
   );
 };
 
---
+++ data/socials.ts
@@ -0,0 +1,20 @@
+const Socials = [
+  {
+    name: 'GitHub',
+    href: 'https://github.com/codingcodax/',
+  },
+  {
+    name: 'LinkedIn',
+    href: 'https://github.com/codingcodax/',
+  },
+  {
+    name: 'Twitter',
+    href: 'https://twitter.com/codingcodax',
+  },
+  {
+    name: 'YouTube',
+    href: 'https://www.youtube.com/channel/UCMY0GhV1HuX4XdbgalC77VQ',
+  },
+];
+
+export default Socials;
