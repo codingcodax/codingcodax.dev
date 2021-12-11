45a35d7fa609b238830c95ae987079f59126d2bd
Alexis Guzman <alexis12989x@gmail.com>
2021-12-11 15:45:20 -0600

    feat(data): add icons to social objects

--- data/socials.tsx
+++ data/socials.tsx
@@ -1,19 +1,30 @@
+import {
+  GitHubIcon,
+  LinkedInIcon,
+  TwitterIcon,
+  YouTubeIcon,
+} from '@/components/icons';
+
 const Socials = [
   {
     name: 'GitHub',
     href: 'https://github.com/codingcodax/',
+    icon: <GitHubIcon css={{ h: '$6' }} />,
   },
   {
     name: 'LinkedIn',
     href: 'https://github.com/codingcodax/',
+    icon: <LinkedInIcon css={{ h: '$6' }} />,
   },
   {
     name: 'Twitter',
     href: 'https://twitter.com/codingcodax',
+    icon: <TwitterIcon css={{ h: '$6' }} />,
   },
   {
     name: 'YouTube',
     href: 'https://www.youtube.com/channel/UCMY0GhV1HuX4XdbgalC77VQ',
+    icon: <YouTubeIcon css={{ h: '$6' }} />,
   },
 ];
 
