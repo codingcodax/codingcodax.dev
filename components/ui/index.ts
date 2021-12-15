ddb743be91d1319b4f011231c4fa64ea02d43ea9
Alexis Guzman <alexis12989x@gmail.com>
2021-12-14 18:18:51 -0600

    feat(ui): import a stitch component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,6 @@
 export { default as Heading } from './Heading';
 export { default as Text } from './Text';
+export { default as A } from './A';
 export { default as Box } from './Box';
 export { default as Center } from './Center';
 export { default as Grid } from './Grid';
