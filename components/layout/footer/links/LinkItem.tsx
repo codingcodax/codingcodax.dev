39a3945240ee448cd71593ac7ea4e3ed20b00f65
Alexis Guzman <alexis12989x@gmail.com>
2021-12-14 19:00:41 -0600

    feat(footer): add is external prop and render jsx dynamiclly

--- components/layout/footer/links/LinkItem.tsx
+++ components/layout/footer/links/LinkItem.tsx
@@ -7,15 +7,31 @@ import Link from '@/components/common/Link';
 interface LinkItemProps {
   href: string;
   children: ReactNode;
+  isExternal?: boolean;
 }
 
-const LinkItem: FC<LinkItemProps> = ({ href, children }) => {
+const LinkItem: FC<LinkItemProps> = ({
+  href,
+  children,
+  isExternal = false,
+}) => {
   return (
     <li>
       <Link href={href}>
-        <A textDecoration='none' type='footer-link'>
-          {children}
-        </A>
+        {isExternal ? (
+          <A
+            target='_blank'
+            rel='noopener noreferrer'
+            textDecoration='none'
+            type='footer-link'
+          >
+            {children}
+          </A>
+        ) : (
+          <A textDecoration='none' type='footer-link'>
+            {children}
+          </A>
+        )}
       </Link>
     </li>
   );
