870a866b4c8c6d6087e1cdf72d99221f959fda8f
Alexis Guzman <alexis12989x@gmail.com>
2021-12-15 22:28:35 -0600

    feat(ui): add relaxed line height variant

--- components/ui/Text.tsx
+++ components/ui/Text.tsx
@@ -3,7 +3,13 @@ import { styled } from '@/theme/theme.config';
 const Text = styled('p', {
   color: '$textColor',
   fontSize: '$base',
-  lineHeight: '$relaxed',
+  lineHeight: '$normal',
+
+  variants: {
+    lineHeight: {
+      relaxed: { lineHeight: '$relaxed' },
+    },
+  },
 });
 
 Text.displayName = 'Text';
