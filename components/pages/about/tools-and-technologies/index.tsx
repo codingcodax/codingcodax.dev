387ac1de3fadb7a856c489a6f70a29fd335ef225
Alexis Guzman <alexis12989x@gmail.com>
2021-12-17 10:49:25 -0600

    feat(about): import section heading component

--- components/pages/about/tools-and-technologies/index.tsx
+++ components/pages/about/tools-and-technologies/index.tsx
@@ -1,8 +1,18 @@
+import { Box, Text } from '@/components/ui';
+
+import SectionHeading from '../common/SectionHeading';
+
 const ToolsAndTechnologies = () => {
   return (
-    <div>
-      <p>Tools And Technologies Component</p>
-    </div>
+    <Box css={{ w: '$full', maxWidth: '$container-sm' }} d>
+      <SectionHeading>Tools & Technologies</SectionHeading>
+      <Text lineHeight='relaxed'>
+        One of my favorite things since I starting to learn to code has been to
+        see all of the amazing tools other humans have created to make other
+        humans{"'"} lives easier. Here are some of the tools I utilise most
+        regularly.
+      </Text>
+    </Box>
   );
 };
 
