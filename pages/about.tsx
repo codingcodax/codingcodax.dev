12340b0deadfca738f80ac5f052dafc360f0f3e2
Alexis Guzman <alexis12989x@gmail.com>
2021-12-17 10:32:01 -0600

    feat(about): import tools and technologies component

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,11 +3,13 @@ import type { NextPage } from 'next';
 import { Center } from '@/components/ui/';
 
 import Intro from '@/components/pages/about/Intro';
+import ToolsAndTechnologies from '@/components/pages/about/tools-and-technologies';
 
 const About: NextPage = () => {
   return (
-    <Center>
+    <Center css={{ flexDirection: 'column' }}>
       <Intro />
+      <ToolsAndTechnologies />
     </Center>
   );
 };
