8ce92b48ac296b9de82c6698cb24e10fe1f269e3
Alexis Guzman <alexis12989x@gmail.com>
2022-01-07 12:07:44 -0600

    feat(about): setup skills component

--- components/pages/about/tools-and-technologies/skills/index.tsx
+++ components/pages/about/tools-and-technologies/skills/index.tsx
@@ -1,8 +1,28 @@
+import { Grid } from '@/components/ui';
+
+import { Tool } from 'types';
+
+import * as tools from '@/data/tools';
+
+import Item from './Item';
+
 const Skills = () => {
+  const skills = Object.values(tools);
+
   return (
-    <div>
-      <p>Skills Component</p>
-    </div>
+    <Grid
+      as='ul'
+      css={{
+        gap: '$6',
+        gridTemplateColumns: 'repeat(2, 1fr)',
+        '@bp1': { gridTemplateColumns: 'repeat(3, 1fr)' },
+        '@bp2': { gridTemplateColumns: 'repeat(4, 1fr)' },
+      }}
+    >
+      {skills.map((skill: Tool) => (
+        <Item key={skill.name} name={skill.name} icon={skill.icon} />
+      ))}
+    </Grid>
   );
 };
 
