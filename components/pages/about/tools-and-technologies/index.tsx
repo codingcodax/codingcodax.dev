51b59b0d3b738826338daa935da22089cca53f9d
Alexis Guzman <alexis12989x@gmail.com>
2022-01-07 12:10:19 -0600

    feat(about): wrap text and skills components in a box component

--- components/pages/about/tools-and-technologies/index.tsx
+++ components/pages/about/tools-and-technologies/index.tsx
@@ -1,18 +1,22 @@
 import { Box, Text } from '@/components/ui';
 
 import SectionHeading from '../common/SectionHeading';
+import Skills from './skills';
 
 const ToolsAndTechnologies = () => {
   return (
-    <Box css={{ w: '$full', maxWidth: '$container-sm' }} d>
+    <>
       <SectionHeading>Tools & Technologies</SectionHeading>
-      <Text lineHeight='relaxed'>
-        One of my favorite things since I starting to learn to code has been to
-        see all of the amazing tools other humans have created to make other
-        humans{"'"} lives easier. Here are some of the tools I utilise most
-        regularly.
-      </Text>
-    </Box>
+      <Box align='center' css={{ mb: '$20', maxWidth: '$container-sm' }}>
+        <Text lineHeight='relaxed' css={{ mb: '$20' }}>
+          One of my favorite things since I starting to learn to code has been
+          to see all of the amazing tools other humans have created to make
+          other humans{"'"} lives easier. Here are some of the tools I utilise
+          most regularly.
+        </Text>
+        <Skills />
+      </Box>
+    </>
   );
 };
 
