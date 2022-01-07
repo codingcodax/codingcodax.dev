2aa696dc281a016a5ec7e3f9443974acb2b1dd41
Alexis Guzman <alexis12989x@gmail.com>
2022-01-07 11:15:37 -0600

    feat(data): change h from 6 to full

--- data/tools.tsx
+++ data/tools.tsx
@@ -19,70 +19,70 @@ import { Tool } from 'types';
 
 export const html: Tool = {
   name: 'HTML5',
-  icon: <HtmlIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <HtmlIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const css: Tool = {
   name: 'CSS3',
-  icon: <CSSIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <CSSIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const sass: Tool = {
   name: 'SASS',
-  icon: <SASSIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <SASSIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const javascript: Tool = {
   name: 'JavaScript',
-  icon: <JavaScriptIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <JavaScriptIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const typescript: Tool = {
   name: 'TypeScript',
-  icon: <TypeScriptIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <TypeScriptIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const reactjs: Tool = {
   name: 'React JS',
-  icon: <ReactJSIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <ReactJSIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const nextjs: Tool = {
   name: 'Next JS',
-  icon: <NextJSIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <NextJSIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const nodejs: Tool = {
   name: 'Node JS',
-  icon: <NodeJSIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <NodeJSIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const chakraui: Tool = {
   name: 'Chakra UI',
-  icon: <ChakraUIIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <ChakraUIIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const firebase: Tool = {
   name: 'Firebase',
-  icon: <FirebaseIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <FirebaseIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const storybook: Tool = {
   name: 'Storybook',
-  icon: <StorybookIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <StorybookIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const mdx: Tool = {
   name: 'MDX',
-  icon: <MDXIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <MDXIcon css={{ color: '$headingColor' }} />,
 };
 
 export const alanai: Tool = {
   name: 'MDX',
-  icon: <AlanAIIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <AlanAIIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
 
 export const figma: Tool = {
   name: 'Figma',
-  icon: <FigmaIcon css={{ h: '$6', color: '$headingColor' }} />,
+  icon: <FigmaIcon css={{ h: '$full', color: '$headingColor' }} />,
 };
