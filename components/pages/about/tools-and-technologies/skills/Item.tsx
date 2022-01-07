b7e4756de988dc5fe6106ca574e0dc691a454b7f
Alexis Guzman <alexis12989x@gmail.com>
2022-01-07 12:06:07 -0600

    feat(about): setup item component

--- components/pages/about/tools-and-technologies/skills/Item.tsx
+++ components/pages/about/tools-and-technologies/skills/Item.tsx
@@ -1,8 +1,24 @@
-const Item = () => {
+import { Center, Grid } from '@/components/ui';
+import { FC, ReactNode } from 'react';
+
+interface ItemProps {
+  name: string;
+  icon: ReactNode;
+}
+
+const Item: FC<ItemProps> = ({ icon, name }) => {
   return (
-    <div>
-      <p>Skill Item Component</p>
-    </div>
+    <Grid
+      as='li'
+      css={{
+        gridRowGap: '$2',
+        justifyItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Center css={{ h: '$12', w: '$12' }}>{icon}</Center>
+      {name}
+    </Grid>
   );
 };
 
