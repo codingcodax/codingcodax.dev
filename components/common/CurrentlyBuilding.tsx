f0ba150a391612a4f09c74a1f6d7a26902829b8d
Alexis Guzman <alexis12989x@gmail.com>
2022-01-10 13:23:08 -0600

    feat(common): create and setup currently building component

---
+++ components/common/CurrentlyBuilding.tsx
@@ -0,0 +1,28 @@
+import { FC, ReactNode } from 'react';
+
+import { Center, Grid } from '../ui';
+
+interface CurrentlyBuildingProps {
+  children: ReactNode;
+}
+
+const CurrentlyBuilding: FC<CurrentlyBuildingProps> = ({ children }) => {
+  return (
+    <Center>
+      <Grid
+        css={{
+          p: '$8',
+          h: '$min',
+          maxWidth: '550px',
+          gridRowGap: '$8',
+          border: '2px solid $dividerColor',
+          borderRadius: '$sm',
+        }}
+      >
+        {children}
+      </Grid>
+    </Center>
+  );
+};
+
+export default CurrentlyBuilding;
