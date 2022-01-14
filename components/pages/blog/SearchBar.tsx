ca5f6d8843c27d32f4c54d7f9b688bdc0bf42497
Alexis Guzman <alexis12989x@gmail.com>
2022-01-14 11:49:29 -0600

    feat(blog): setup search bar component

--- components/pages/blog/SearchBar.tsx
+++ components/pages/blog/SearchBar.tsx
@@ -1,8 +1,31 @@
-const SearchBar = () => {
+import { ChangeEventHandler, FC } from 'react';
+
+import { SearchIcon } from '@/components/icons';
+import { Grid, Input } from '@/components/ui';
+
+interface SearchBarProps {
+  value: string;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+const SearchBar: FC<SearchBarProps> = ({ value, handleChange }) => {
   return (
-    <div>
-      <p>SearchBar Component</p>
-    </div>
+    <Grid direction='column' css={{ mt: '$4', position: 'relative' }}>
+      <Input
+        value={value}
+        onChange={handleChange}
+        placeholder='Search articles...'
+      />
+      <SearchIcon
+        css={{
+          p: '$3',
+          h: '$full',
+          position: 'absolute',
+          top: '0',
+          right: '0',
+        }}
+      />
+    </Grid>
   );
 };
 
