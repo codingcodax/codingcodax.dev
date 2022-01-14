302384b9b0463b0424e0035cab0e87beb25a8fa0
Alexis Guzman <alexis12989x@gmail.com>
2022-01-14 12:02:16 -0600

    feat(types): create post meta and post type

---
+++ types/data/post.ts
@@ -0,0 +1,12 @@
+export type PostMeta = {
+  title: string;
+  publishedAt: string;
+  publishedAtFormatted: string;
+  description: string;
+  slug?: any;
+  category?: 'draft' | 'project' | 'post';
+  image?: string;
+  source?: string;
+};
+
+export type Post = { meta: PostMeta; code?: any };
