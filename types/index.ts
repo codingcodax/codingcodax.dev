ba8c6da5951d62b8060d13780a48b59b42834273
Alexis Guzman <alexis12989x@gmail.com>
2022-01-14 17:18:42 -0600

    feat(types): import PostMeta type

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
 export type { default as ToggleMenu } from './hooks/toggleMenu';
 export type { default as Tool } from './data/tool';
 export type { default as Project } from './data/project';
+export type { PostMeta } from './data/post';
