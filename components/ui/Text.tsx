dd055bebb0a6732466f50ca9c43de520be4032c4
Alexis Guzman <alexis12989x@gmail.com>
2022-01-19 16:37:37 -0600

    feat(ui): add sizes in variants

--- components/ui/Text.tsx
+++ components/ui/Text.tsx
@@ -6,6 +6,21 @@ const Text = styled('p', {
   lineHeight: '$normal',
 
   variants: {
+    size: {
+      sm: { fontSize: '$sm' },
+      md: { fontSize: '$md' },
+      lg: { fontSize: '$lg' },
+      xl: { fontSize: '$xl' },
+      '2xl': { fontSize: '$2xl' },
+      '3xl': { fontSize: '$3xl' },
+      '4xl': { fontSize: '$4xl' },
+      '5xl': { fontSize: '$5xl' },
+      '6xl': { fontSize: '$6xl' },
+      '7xl': { fontSize: '$7xl' },
+      '8xl': { fontSize: '$8xl' },
+      '9xl': { fontSize: '$9xl' },
+    },
+
     lineHeight: {
       relaxed: { lineHeight: '$relaxed' },
     },
