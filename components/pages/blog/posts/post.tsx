18b26c7534e2dcc9459333093437ce7997ea868c
Alexis Guzman <alexis12989x@gmail.com>
2022-01-26 17:12:56 -0600

    feat(blog): remove image and add styles

--- components/pages/blog/posts/post.tsx
+++ components/pages/blog/posts/post.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image';
-
-import { Center, Grid, Heading, Text } from '@/components/ui';
+import { Grid, Heading, Text } from '@/components/ui';
 
 import type { PostMeta } from 'types';
 import Link from '@/components/common/Link';
-import { CalendarIcon, ClockIcon } from '@/components/icons';
+import Details from '../Details';
 
 const Post = ({
   title,
@@ -19,82 +17,39 @@ const Post = ({
     <Link href={`/blog/${slug}`}>
       <Grid
         as='a'
-        direction='column'
         css={{
-          gridTemplateColumns: 'auto 1fr',
-          gridColumnGap: '$6',
+          p: '$8',
           textDecoration: 'none',
+          bg: '$mauve200',
+          border: '1px solid $dividerColor',
+          borderRadius: '$sm',
+          transition: 'background ease-in-out $normal',
+
           '&:hover': {
+            bg: '$mauve300',
+
             h2: {
               color: '$primary',
             },
           },
         }}
       >
-        <Center
-          css={{
-            height: '100px',
-            width: '150px',
-            borderRadius: '$sm',
-            overflow: 'hidden',
-          }}
+        <Heading
+          size='xl'
+          fontWeight='bold'
+          css={{ transition: 'color ease-in-out $normal' }}
         >
-          <Image
-            width={600}
-            height={400}
-            alt={title}
-            src={image}
-            placeholder='blur'
-            blurDataURL={imageBlur}
-            objectFit='cover'
-          />
-        </Center>
+          {title}
+        </Heading>
 
-        <Grid css={{ gridTemplateRows: 'auto auto 1fr' }}>
-          <Heading
-            size='xl'
-            fontWeight='bold'
-            css={{ transition: 'color ease-in-out $normal' }}
-          >
-            {title}
-          </Heading>
-          <Text
-            css={{
-              display: '-webkit-box',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              '-webkit-line-clamp': '2',
-              '-webkit-box-orient': 'vertical',
-              fontSize: '$md',
-            }}
-          >
-            {summary}
-          </Text>
+        <Details
+          readingTime={readingTime}
+          publishedAtFormatted={publishedAtFormatted}
+        />
 
-          <Grid
-            direction='column'
-            css={{
-              alignSelf: 'end',
-              justifyContent: 'start',
-              gridColumnGap: '$4',
-              color: '$grayTextColor',
-            }}
-          >
-            <Center>
-              <CalendarIcon css={{ width: '$5', mr: '$1' }} />
-              <Text as='span' size='sm' css={{ color: '$grayTextColor' }}>
-                {readingTime?.text}
-              </Text>
-            </Center>
-
-            <Center>
-              <ClockIcon css={{ w: '$5', mr: '$1' }} />
-              <Text as='span' size='sm' css={{ color: '$grayTextColor' }}>
-                {publishedAtFormatted}
-              </Text>
-            </Center>
-          </Grid>
-        </Grid>
+        <Text size='md' lineHeight='relaxed'>
+          {summary}
+        </Text>
       </Grid>
     </Link>
   );
