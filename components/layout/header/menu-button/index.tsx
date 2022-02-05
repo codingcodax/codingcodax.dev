e98d20ed153a4bc431bf77a0237da8360282e374
Alexis Guzman <alexis12989x@gmail.com>
2022-02-05 11:47:31 -0600

    feat(layout): replace styles with extra variant

--- components/layout/header/menu-button/index.tsx
+++ components/layout/header/menu-button/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@/components/ui';
+import { Button } from '@/components/ui';
 import MenuIcon from './MenuIcon';
 import { FC } from 'react';
 
@@ -9,22 +9,18 @@ interface MenuButtonProps {
 
 const MenuButton: FC<MenuButtonProps> = ({ isMenuOpen, toggleMenu }) => {
   return (
-    <Box
-      as='button'
+    <Button
+      variant='extra'
       css={{
         p: '$0',
         w: '$12',
         h: '$12',
-        bg: 'transparent',
-        border: 'none',
-        borderRadius: '$sm',
-        cursor: 'pointer',
       }}
       aria-label='menu toggle button'
       onClick={toggleMenu}
     >
       <MenuIcon isOpen={isMenuOpen} />
-    </Box>
+    </Button>
   );
 };
 
