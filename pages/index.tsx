eaed9648adb80f4ef9fa5808c3f115fd1a304a1c
Alexis Guzman <alexis12989x@gmail.com>
2022-02-05 11:49:00 -0600

    feat(home): remove newsletter component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import type { NextPage } from 'next';
 
 import Hero from '@/components/pages/home/hero';
 import FeaturedProjects from '@/components/pages/home/FeaturedProjects';
-import Newsletter from '@/components/pages/home/Newsletter';
+// import Newsletter from '@/components/pages/home/Newsletter';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Hero />
       <FeaturedProjects />
-      <Newsletter />
+      {/* <Newsletter /> */}
     </div>
   );
 };
