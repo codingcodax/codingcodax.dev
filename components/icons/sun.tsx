05bff558a937661894130c48d3dfb06f011940ef
Alexis Guzman <alexis12989x@gmail.com>
2022-02-05 11:49:32 -0600

    feat(icons): animate icons with framer

--- components/icons/sun.tsx
+++ components/icons/sun.tsx
@@ -1,21 +1,62 @@
+import { motion } from 'framer-motion';
+
 import { Svg } from '../ui';
+import { coreVariants, raysVariants, whileTap } from 'animations/toggleTheme';
+
+const MotionSvg = motion(Svg);
 
 const Sun = ({ ...props }) => {
   return (
-    <Svg
+    <MotionSvg
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
-      stroke='currentColor'
       viewBox='0 0 24 24'
+      whileTap={whileTap}
       {...props}
     >
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth='2'
-        d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+      <motion.circle
+        cx='11.9998'
+        cy='11.9998'
+        r='5.75375'
+        fill='currentColor'
+        variants={coreVariants}
+        initial='initial'
+        animate='animate'
       />
-    </Svg>
+
+      <motion.g initial='initial' animate='animate' variants={raysVariants}>
+        <circle
+          cx='3.08982'
+          cy='6.85502'
+          r='1.71143'
+          transform='rotate(-60 3.08982 6.85502)'
+          fill='currentColor'
+        />
+        <circle
+          cx='3.0903'
+          cy='17.1436'
+          r='1.71143'
+          transform='rotate(-120 3.0903 17.1436)'
+          fill='currentColor'
+        />
+        <circle cx='12' cy='22.2881' r='1.71143' fill='currentColor' />
+        <circle
+          cx='20.9101'
+          cy='17.1436'
+          r='1.71143'
+          transform='rotate(-60 20.9101 17.1436)'
+          fill='currentColor'
+        />
+        <circle
+          cx='20.9101'
+          cy='6.8555'
+          r='1.71143'
+          transform='rotate(-120 20.9101 6.8555)'
+          fill='currentColor'
+        />
+        <circle cx='12' cy='1.71143' r='1.71143' fill='currentColor' />
+      </motion.g>
+    </MotionSvg>
   );
 };
 
