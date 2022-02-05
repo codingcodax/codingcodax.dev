cd600f8c0e6bd1cc7f9183e63cbf1618ff9243e3
Alexis Guzman <alexis12989x@gmail.com>
2022-02-05 16:58:24 -0600

    feat(404): create 404 page component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,17 @@
 import { NextPage } from 'next';
 
+import { Grid } from '@/components/ui';
+
 const Custom404: NextPage = () => {
   return (
-    <div>
-      <p>404 Page Component</p>
-    </div>
+    <Grid direction='column'>
+      <div>
+        <h3>Oops!</h3>
+        <h1>We couldn{"'"}t find that page</h1>
+        <h2>Maybe you can find what you need here?</h2>
+      </div>
+      <div></div>
+    </Grid>
   );
 };
 
