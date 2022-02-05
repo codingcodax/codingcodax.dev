b3a3b64c6cb89514f8816c529968ce1e980ba219
Alexis Guzman <alexis12989x@gmail.com>
2022-02-05 11:40:24 -0600

    feat(layout): add onclick function to all nav items

--- components/layout/header/nav/MobileNav.tsx
+++ components/layout/header/nav/MobileNav.tsx
@@ -25,15 +25,27 @@ const MobileNav: FC<MobileNavProps> = ({ toggleMenu }) => {
         <NavItem href='/' onClick={toggleMenu}>
           Home
         </NavItem>
-        <NavItem href='/about'>About</NavItem>
-        <NavItem href='/projects'>Projects</NavItem>
-        <NavItem href='/blog'>Blog</NavItem>
+        <NavItem href='/about' onClick={toggleMenu}>
+          About
+        </NavItem>
+        <NavItem href='/projects' onClick={toggleMenu}>
+          Projects
+        </NavItem>
+        <NavItem href='/blog' onClick={toggleMenu}>
+          Blog
+        </NavItem>
 
         <Divider css={{ width: '50vw' }} />
 
-        <NavItem href='/snippets'>Snippets</NavItem>
-        <NavItem href='/community'>Community</NavItem>
-        <NavItem href='/uses'>Uses</NavItem>
+        <NavItem href='/snippets' onClick={toggleMenu}>
+          Snippets
+        </NavItem>
+        <NavItem href='/community' onClick={toggleMenu}>
+          Community
+        </NavItem>
+        <NavItem href='/uses' onClick={toggleMenu}>
+          Uses
+        </NavItem>
       </Box>
     </Box>
   );
