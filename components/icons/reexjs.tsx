b85d162e7461de2970b7609d485536deed3eb6a8
Alexis Guzman <alexis12989x@gmail.com>
2022-02-07 14:15:15 -0600

    fix(icons): change view box prop

--- components/icons/reexjs.tsx
+++ components/icons/reexjs.tsx
@@ -5,7 +5,7 @@ const ReexJS = ({ ...props }) => {
     <Svg
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
-      viewBox='0 0 24 24'
+      viewBox='0 0 50 50'
       {...props}
     >
       <defs />
