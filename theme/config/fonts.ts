475f9ad829eebbcb671c8939fa70116f4fae43be
Alexis Guzman <alexis12989x@gmail.com>
2022-02-07 10:58:04 -0600

    fix(theme): remove single quotes of fira code font

--- theme/config/fonts.ts
+++ theme/config/fonts.ts
@@ -2,7 +2,7 @@ const fonts = {
   heading:
     '    -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";',
   body: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
-  mono: "'Fira Code', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+  mono: "Fira Code, Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
 };
 
 export default fonts;
