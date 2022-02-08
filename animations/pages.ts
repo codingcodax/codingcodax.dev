6846eaf8480dfe0448ab96b76ff749bf0c7c3c66
Alexis Guzman <alexis12989x@gmail.com>
2022-02-08 12:38:38 -0600

    feat(animations): change animations name

---
+++ animations/pages.ts
@@ -0,0 +1,10 @@
+export const inputVariants = {
+  hidden: { y: 50, opacity: 0 },
+  show: { y: 0, opacity: 1 },
+  exit: { y: -50, opacity: 0 },
+};
+
+export const outputVariants = {
+  hidden: { y: -50, opacity: 0 },
+  show: { y: 0, opacity: 1 },
+};
