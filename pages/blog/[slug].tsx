1d6755300911230c03b5c03201cdd24edf1333bb
Alexis Guzman <alexis12989x@gmail.com>
2022-02-08 13:11:31 -0600

    feat(framer): add input variants

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,9 +7,10 @@ import type { Post as PostType } from 'types';
 
 import { getAllPostsMeta, getPostBySlug } from '@/lib/mdx';
 import { components } from '@/components/pages/blog/mdx-components';
-import { Box, Heading, Text } from '@/components/ui';
+import { Box, Heading, MotionBox, Text } from '@/components/ui';
 import Details from '@/components/pages/blog/Details';
 import ProgressBar from '@/components/pages/blog/ProgressBar';
+import { inputVariants } from 'animations/pages';
 
 export const getStaticPaths = () => {
   const posts = getAllPostsMeta('post');
@@ -42,97 +43,102 @@ const Post = ({ meta, code }: PostType) => {
   const Component = useMemo(() => getMDXComponent(code), [code]);
 
   return (
-    <Box
-      align='center'
-      css={{
-        maxWidth: '800px',
-        position: 'relative',
-
-        '.rehype-code-title': {
-          pt: '$2',
-          pb: '$1',
-          px: '$4',
-          mt: '$4',
-          ml: '$4',
-          '@bp1': { marginLeft: '10%' },
-          width: 'fit-content',
-          fontSize: '$sm',
-          fontWeight: '$bold',
-          bg: '$subtleBackground',
-          border: '1px solid $subtleBorder',
-          borderBottom: 'none',
-          borderTopLeftRadius: '$sm',
-          borderTopRightRadius: '$sm',
+    <>
+      <ProgressBar />
+      <MotionBox
+        align='center'
+        css={{
+          maxWidth: '800px',
           position: 'relative',
 
-          '&::after': {
-            content: '',
-            height: '1px',
-            width: '100%',
+          '.rehype-code-title': {
+            pt: '$2',
+            pb: '$1',
+            px: '$4',
+            mt: '$4',
+            ml: '$4',
+            '@bp1': { marginLeft: '10%' },
+            width: 'fit-content',
+            fontSize: '$sm',
+            fontWeight: '$bold',
             bg: '$subtleBackground',
-            position: 'absolute',
-            left: '0',
-            bottom: '-1px',
-            zIndex: '$docked',
+            border: '1px solid $subtleBorder',
+            borderBottom: 'none',
+            borderTopLeftRadius: '$sm',
+            borderTopRightRadius: '$sm',
+            position: 'relative',
+
+            '&::after': {
+              content: '',
+              height: '1px',
+              width: '100%',
+              bg: '$subtleBackground',
+              position: 'absolute',
+              left: '0',
+              bottom: '-1px',
+              zIndex: '$docked',
+            },
           },
-        },
-        '.rehype-code-title + div.pre-container': {
-          mt: '$0',
-        },
+          '.rehype-code-title + div.pre-container': {
+            mt: '$0',
+          },
 
-        '.toc': {
-          p: '$4',
-          my: '$8',
-          mx: '$auto',
-          maxWidth: '$container-sm',
-          bg: '$subtleBackground',
-          borderRadius: '$sm',
-        },
-        '.toc>p.title': {
-          fontSize: '$md',
-          fontWeight: '$bold',
-          textTransform: 'uppercase',
-          letterSpacing: '2px',
-        },
-        '.toc>ol': { p: '$0' },
-        '.toc>ol>div a': { p: '$0', textDecoration: 'none' },
-        '.toc>ol>div::before': { color: '$lowTextColor' },
-      }}
-    >
-      <Heading
-        as='h1'
-        css={{
-          mx: '$auto',
-          mt: '$20',
-          mb: '$2',
-          maxWidth: '$container-sm',
-          lineHeight: '$none',
+          '.toc': {
+            p: '$4',
+            my: '$8',
+            mx: '$auto',
+            maxWidth: '$container-sm',
+            bg: '$subtleBackground',
+            borderRadius: '$sm',
+          },
+          '.toc>p.title': {
+            fontSize: '$md',
+            fontWeight: '$bold',
+            textTransform: 'uppercase',
+            letterSpacing: '2px',
+          },
+          '.toc>ol': { p: '$0' },
+          '.toc>ol>div a': { p: '$0', textDecoration: 'none' },
+          '.toc>ol>div::before': { color: '$lowTextColor' },
         }}
+        variants={inputVariants}
+        initial='hidden'
+        animate='show'
       >
-        {meta.title}
-      </Heading>
+        <Heading
+          as='h1'
+          css={{
+            mx: '$auto',
+            mt: '$20',
+            mb: '$2',
+            maxWidth: '$container-sm',
+            lineHeight: '$none',
+          }}
+        >
+          {meta.title}
+        </Heading>
 
-      <Box align='center' css={{ maxWidth: '$container-sm' }}>
-        <Details
-          readingTime={meta.readingTime}
-          publishedAtFormatted={meta.publishedAtFormatted}
-        />
-      </Box>
+        <Box align='center' css={{ maxWidth: '$container-sm' }}>
+          <Details
+            readingTime={meta.readingTime}
+            publishedAtFormatted={meta.publishedAtFormatted}
+          />
+        </Box>
 
-      <Text
-        css={{
-          mt: '$4',
-          mb: '$10',
-          mx: '$auto',
-          maxWidth: '$container-sm',
-          color: '$lowTextColor',
-        }}
-      >
-        {meta.summary}
-      </Text>
-      <ProgressBar />
-      <Component components={components} />
-    </Box>
+        <Text
+          css={{
+            mt: '$4',
+            mb: '$10',
+            mx: '$auto',
+            maxWidth: '$container-sm',
+            color: '$lowTextColor',
+          }}
+        >
+          {meta.summary}
+        </Text>
+        <Component components={components} />
+      </MotionBox>
+    </>
   );
 };
 
