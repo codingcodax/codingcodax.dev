b565777ff6ff9891bc9c422e79bd3f7b618ae87e
Alexis Guzman <alexis12989x@gmail.com>
2022-02-09 10:16:48 -0600

    feat(404): create and setup 404 page container

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,17 +1,53 @@
 import { NextPage } from 'next';
 
-import { Grid } from '@/components/ui';
+import { Button, Center, Box, Text, Heading } from '@/components/ui';
+import Link from '@/components/common/Link';
 
 const Custom404: NextPage = () => {
   return (
-    <Grid direction='column'>
-      <div>
-        <h3>Oops!</h3>
-        <h1>We couldn{"'"}t find that page</h1>
-        <h2>Maybe you can find what you need here?</h2>
-      </div>
-      <div></div>
-    </Grid>
+    <Box css={{ position: 'relative' }}>
+      <Center css={{ w: '$full', position: 'absolute' }}>
+        <Text
+          css={{
+            fontSize: '600px',
+            color: '$subtleBackground',
+            lineHeight: '$none',
+          }}
+        >
+          404
+        </Text>
+      </Center>
+
+      <Center
+        css={{
+          mx: '$auto',
+          h: '$full',
+          maxWidth: '$100px',
+          flexDirection: 'column',
+          alignItems: 'start',
+          position: 'relative',
+          zIndex: '$modal',
+        }}
+      >
+        <Heading size='9xl' fontWeight='bold'>
+          Oops!
+        </Heading>
+        <Heading as='h1' fontWeight='normal' css={{ mb: '$10' }}>
+          Looks like something went wrong and the page you are looking can{"'"}t
+          be found
+        </Heading>
+        <Link href='/'>
+          <Button
+            as='a'
+            variant='primary'
+            size='large'
+            css={{ textDecoration: 'none' }}
+          >
+            Let{"'"}s go back
+          </Button>
+        </Link>
+      </Center>
+    </Box>
   );
 };
 
