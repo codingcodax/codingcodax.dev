e223697178dd3cb1642a543b0cda0ae0f277a95a
Alexis Guzman <alexis12989x@gmail.com>
2022-02-11 18:13:08 -0600

    feat(head): add meta and link tags for seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,33 @@ export default class Document extends NextDocument {
     return (
       <Html lang='en'>
         <Head>
+          <meta content='IE=edge' httpEquiv='X-UA-Compatible' />
+          <meta content='width=device-width, initial-scale=1' name='viewport' />
+          <meta content='#6e56cf' name='theme-color' />
+          <meta content='#ededef' name='msapplication-TileColor' />
+          <meta
+            content='/favicons/browserconfig.xml'
+            name='msapplication-config'
+          />
+          <link href='/favicons/favicon.ico' rel='shortcut icon' />
+          <link href='/favicons/site.webmanifest' rel='manifest' />
+          <link
+            href='/favicons/apple-touch-icon.png'
+            rel='apple-touch-icon'
+            sizes='180x180'
+          />
+          <link
+            href='/favicons/favicon-32x32.png'
+            rel='icon'
+            sizes='32x32'
+            type='image/png'
+          />
+          <link
+            href='/favicons/favicon-16x16.png'
+            rel='icon'
+            sizes='16x16'
+            type='image/png'
+          />
           <style
             id='stitches'
             dangerouslySetInnerHTML={{ __html: getCssText() }}
