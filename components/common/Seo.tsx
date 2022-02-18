e9f0173f10da61ac4887b48370513c62b6d9edca
Alexis Guzman <alexis12989x@gmail.com>
2022-02-18 17:03:47 -0600

    feat(common): add description and og image props

--- components/common/Seo.tsx
+++ components/common/Seo.tsx
@@ -1,18 +1,37 @@
 import { FC } from 'react';
 import { NextSeo } from 'next-seo';
 
-import { domain } from 'next-seo.config';
+import { description as defaultDescription, domain } from 'next-seo.config';
 
 interface SeoProps {
   name: string;
   path: string;
+  description?: string;
+  ogImage?: string;
 }
 
-const Seo: FC<SeoProps> = ({ name, path }) => {
+const Seo: FC<SeoProps> = ({
+  name,
+  path,
+  description = defaultDescription,
+  ogImage = domain + '/og.png',
+}) => {
   const title = `${name} | Alexis Guzman`;
   const url = `${domain}${path}`;
 
-  return <NextSeo title={title} canonical={url} openGraph={{ url, title }} />;
+  return (
+    <NextSeo
+      title={title}
+      description={description}
+      canonical={url}
+      openGraph={{
+        url,
+        title,
+        description,
+        images: [{ url: ogImage, alt: title, height: 720, width: 1280 }],
+      }}
+    />
+  );
 };
 
 export default Seo;
