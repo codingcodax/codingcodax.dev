fed030770aaaf27c7805d4af96e76055872353e2
Alexis Guzman <alexis12989x@gmail.com>
2022-02-18 16:17:52 -0600

    feat(types): import socials type

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,4 @@ export type { default as ToggleMenu } from './hooks/toggleMenu';
 export type { default as Tool } from './data/tool';
 export type { default as Project } from './data/project';
 export type { PostMeta, Post } from './data/post';
+export type { default as Socials } from './data/socials';
