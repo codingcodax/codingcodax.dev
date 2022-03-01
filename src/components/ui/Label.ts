5b76fb697fd5d1e0392e88d567a5a136564de7d2
Alexis Guzman <alexis12989x@gmail.com>
2022-02-28 18:09:05 -0600

    feat(ui): create and setup label ui component with radix ui

---
+++ src/components/ui/Label.ts
@@ -0,0 +1,9 @@
+import * as LabelPrimitive from '@radix-ui/react-label';
+
+import { styled } from '~/theme/config';
+
+const Label = styled(LabelPrimitive.Root);
+
+Label.displayName = 'Label';
+
+export default Label;
