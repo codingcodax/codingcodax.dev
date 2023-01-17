99a086b519cb345cb6d1f8af8a46b2fc95335664
Alexis Guzman <codingcodax@gmail.com>
2023-01-17 16:28:42 -0600

    fix(projects): reorder

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -14,24 +14,14 @@ import {
 
 const projects: Project[] = [
   {
-    logo: <WeebnIcon css={{ h: '$12', color: '$backgroundColor' }} />,
-    title: 'weebn',
+    logo: <DuckDuckGo css={{ h: '$12', color: '$backgroundColor' }} />,
+    title: 'DDG Themes',
     description:
-      'Level up your Notion with light weight weeb widgets fully cuztomizable and ready to use.',
-    tools: [nextjs, tailwindcss, headlessui],
-    live: 'https://weebn.codingcodax.dev/',
-    repo: 'https://github.com/codingcodax/weebn/',
+      'Customize DuckDuckGo with your favorite theme, choose from a number of themes and install it.',
+    tools: [nextjs, tailwindcss, radixui],
+    live: 'https://ddg.codingcodax.dev/',
+    repo: 'https://github.com/codingcodax/ddg-themes/',
     featured: false,
-  },
-  {
-    logo: <SlugIcon css={{ h: '$12', color: '$backgroundColor' }} />,
-    title: 'Slug',
-    description:
-      'slug is an open source link managment platform to short your links made for devs.',
-    tools: [nextjs, tailwindcss, typescript, trpc, prisma],
-    live: 'https://slug.codingcodax.dev/',
-    repo: 'https://github.com/codingcodax/slug/',
-    featured: true,
   },
   {
     logo: <MiniIcon css={{ h: '$12', color: '$backgroundColor' }} />,
@@ -44,13 +34,23 @@ const projects: Project[] = [
     featured: true,
   },
   {
-    logo: <DuckDuckGo css={{ h: '$12', color: '$backgroundColor' }} />,
-    title: 'DDG Themes',
+    logo: <SlugIcon css={{ h: '$12', color: '$backgroundColor' }} />,
+    title: 'Slug',
     description:
-      'Customize DuckDuckGo with your favorite theme, choose from a number of themes and install it.',
-    tools: [nextjs, tailwindcss, radixui],
-    live: 'https://ddg.codingcodax.dev/',
-    repo: 'https://github.com/codingcodax/ddg-themes/',
+      'slug is an open source link managment platform to short your links made for devs.',
+    tools: [nextjs, tailwindcss, typescript, trpc, prisma],
+    live: 'https://slug.codingcodax.dev/',
+    repo: 'https://github.com/codingcodax/slug/',
+    featured: true,
+  },
+  {
+    logo: <WeebnIcon css={{ h: '$12', color: '$backgroundColor' }} />,
+    title: 'weebn',
+    description:
+      'Level up your Notion with light weight weeb widgets fully cuztomizable and ready to use.',
+    tools: [nextjs, tailwindcss, headlessui],
+    live: 'https://weebn.codingcodax.dev/',
+    repo: 'https://github.com/codingcodax/weebn/',
     featured: false,
   },
 ];
