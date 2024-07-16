50eb925ce59c84f50bfa3cd2377dd0793b60994b
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 17:21:39 -0600

    feat(components): setup navbar

--- apps/www/src/app/_components/Header/Navbar.tsx
+++ apps/www/src/app/_components/Header/Navbar.tsx
@@ -1,7 +1,12 @@
+import { NavItem } from './NavItem';
+
 export const Navbar = () => {
   return (
-    <div>
-      <p>navbar</p>
+    <div className='flex gap-2'>
+      <NavItem href='/'>Home</NavItem>
+      <NavItem href='/work'>Work</NavItem>
+      <NavItem href='/about'>About</NavItem>
+      <NavItem href='/contact'>Contact</NavItem>
     </div>
   );
 };
