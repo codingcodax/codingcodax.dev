8161bf214af3b66942a88e74d14332481757cb88
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 17:21:35 -0600

    feat(components): setup nav item

--- apps/www/src/app/_components/Header/NavItem.tsx
+++ apps/www/src/app/_components/Header/NavItem.tsx
@@ -1,7 +1,27 @@
-export const NavItem = () => {
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { Button } from '@acme/ui/Button';
+
+interface Props {
+  className?: string;
+  href: string;
+  children: React.ReactNode;
+}
+
+export const NavItem = ({ className, href, children }: Props) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
   return (
-    <div>
-      <p>nav item</p>
-    </div>
+    <Button
+      asChild
+      className={className}
+      variant={isActive ? 'solid' : 'ghost'}
+    >
+      <Link href={href}>{children}</Link>
+    </Button>
   );
 };
