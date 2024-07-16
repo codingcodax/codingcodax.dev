019d9e03f6bf7e2dccdff7e9e4aa27f9d3af36fa
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 09:57:02 -0600

    feat(api): add hello router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,6 +1,9 @@
+import { helloRouter } from './router/hello';
 import { createTRPCRouter } from './trpc';
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  hello: helloRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
