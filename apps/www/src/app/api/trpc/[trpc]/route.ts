883d7d6aa269729245e6c1228df64e35384294f5
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 09:57:52 -0600

    style(prettier): run `format:fix` script

--- apps/www/src/app/api/trpc/[trpc]/route.ts
+++ apps/www/src/app/api/trpc/[trpc]/route.ts
@@ -46,10 +46,10 @@ const handler = async (req: NextRequest) => {
     onError:
       env.NODE_ENV === 'development'
         ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`,
-          );
-        }
+            console.error(
+              `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`,
+            );
+          }
         : undefined,
   });
 
