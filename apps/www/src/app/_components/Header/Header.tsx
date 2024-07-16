b6bfa54909fa4df25fe61f631a9d5c686c577b06
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 17:21:51 -0600

    feat(components): setup header

--- apps/www/src/app/_components/Header/Header.tsx
+++ apps/www/src/app/_components/Header/Header.tsx
@@ -1,7 +1,15 @@
+import { Navbar } from './Navbar';
+
 export const Header = () => {
   return (
-    <div>
-      <p>header</p>
+    <div className='bg-grey-base sticky top-px z-10 mx-auto flex w-full max-w-screen-lg items-center justify-center py-4'>
+      <Navbar />
+
+      {/* borders */}
+      <div className='bg-grey-line absolute -top-px left-0 h-px w-full' />
+      <div className='bg-grey-line absolute -left-px top-0 h-full w-px' />
+      <div className='bg-grey-line absolute -right-px top-0 h-full w-px' />
+      <div className='bg-grey-line absolute -bottom-px left-0 h-px w-full' />
     </div>
   );
 };
