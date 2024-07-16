608c47a20fe04f81a021f5bb95cbcf305940a0d9
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 12:40:29 -0600

    feat(ui): remove `rounded-*` from button component

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -6,7 +6,7 @@ import { cva } from 'class-variance-authority';
 import { cn } from '@acme/ui';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -23,8 +23,8 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
+        sm: 'h-8 px-3 text-xs',
+        lg: 'h-10 px-8',
         icon: 'h-9 w-9',
       },
     },
