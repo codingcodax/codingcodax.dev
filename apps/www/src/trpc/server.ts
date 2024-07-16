60b2f5b7387beca8bd3e5dea4283f1bcb4b685bf
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 09:40:15 -0600

    feat(trpc): remove session

--- apps/www/src/trpc/server.ts
+++ apps/www/src/trpc/server.ts
@@ -1,10 +1,11 @@
+import 'server-only';
+
 import { cache } from 'react';
 import { headers } from 'next/headers';
 import { createHydrationHelpers } from '@trpc/react-query/rsc';
 
 import type { AppRouter } from '@acme/api';
 import { createCaller, createTRPCContext } from '@acme/api';
-import { auth } from '@acme/auth';
 
 import { createQueryClient } from './query-client';
 
@@ -12,12 +13,11 @@ import { createQueryClient } from './query-client';
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(async () => {
+const createContext = cache(() => {
   const heads = new Headers(headers());
   heads.set('x-trpc-source', 'rsc');
 
   return createTRPCContext({
-    session: await auth(),
     headers: heads,
   });
 });
