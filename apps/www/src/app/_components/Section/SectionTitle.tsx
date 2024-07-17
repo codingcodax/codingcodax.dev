e9d1b8370ebbb679472a85dd7353e89340d6ab4f
Alexis Guzman <codingcodax@gmail.com>
2024-07-17 09:12:10 -0600

    feat(components): setup section title

--- apps/www/src/app/_components/Section/SectionTitle.tsx
+++ apps/www/src/app/_components/Section/SectionTitle.tsx
@@ -1,7 +1,5 @@
-export const SectionTitle = () => {
-  return (
-    <div>
-      <p>section title</p>
-    </div>
-  );
+type Props = { children: React.ReactNode };
+
+export const SectionTitle = ({ children }: Props) => {
+  return <h3 className='font-serif text-5xl'>{children}</h3>;
 };
