ed813375f7dfefcdc5d290e9349072a13b234491
Alexis Guzman <codingcodax@gmail.com>
2024-07-17 09:11:51 -0600

    feat(components): setup section description

--- apps/www/src/app/_components/Section/SectionDescription.tsx
+++ apps/www/src/app/_components/Section/SectionDescription.tsx
@@ -1,7 +1,5 @@
-export const SectionDescription = () => {
-  return (
-    <div>
-      <p>section description</p>
-    </div>
-  );
+type Props = { children: React.ReactNode };
+
+export const SectionDescription = ({ children }: Props) => {
+  return <h4 className='font-serif text-2xl text-grey-text'>{children}</h4>;
 };
