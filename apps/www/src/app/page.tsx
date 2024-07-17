94b9cf7d91b1f8886c3be4a405353a9a2e0f8d41
Alexis Guzman <codingcodax@gmail.com>
2024-07-17 09:12:59 -0600

    feat(pages): add work section

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next';
 
 import { Hero } from './_components/Hero';
+import { Section } from './_components/Section';
+import { SectionDescription } from './_components/Section/SectionDescription';
+import { SectionTitle } from './_components/Section/SectionTitle';
 
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'>
       <Hero />
+
+      <Section>
+        <SectionTitle>Work</SectionTitle>
+        <SectionDescription>My latest projects</SectionDescription>
+      </Section>
     </div>
   );
 };
