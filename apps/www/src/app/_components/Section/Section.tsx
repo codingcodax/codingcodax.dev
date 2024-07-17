01a1899bec737e57a4b13765d2e915198b32ac12
Alexis Guzman <codingcodax@gmail.com>
2024-07-17 09:11:44 -0600

    feat(components): setup section

--- apps/www/src/app/_components/Section/Section.tsx
+++ apps/www/src/app/_components/Section/Section.tsx
@@ -1,7 +1,12 @@
-export const Section = () => {
+type Props = { children: React.ReactNode };
+
+export const Section = ({ children }: Props) => {
   return (
-    <div>
-      <p>section</p>
+    <div className='relative'>
+      <div className='flex flex-col gap-1 p-6'>{children}</div>
+
+      <div className='absolute -top-px left-0 h-px w-full bg-grey-line' />
+      <div className='absolute -bottom-px left-0 h-px w-full bg-grey-line' />
     </div>
   );
 };
