d0e6177ab6c415cc45a06c2b8628e5e2d2d06a7d
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 18:03:03 -0600

    feat(components): setup hero

--- apps/www/src/app/_components/Hero/Hero.tsx
+++ apps/www/src/app/_components/Hero/Hero.tsx
@@ -1,7 +1,17 @@
+import { HeroImage } from './HeroImage';
+
 export const Hero = () => {
   return (
-    <div>
-      <p>hero</p>
+    <div className='flex min-h-[50vh] flex-col items-center justify-center px-2 pb-4 pt-8'>
+      <HeroImage />
+
+      <div className='mt-6 flex flex-col items-center justify-center text-center'>
+        <h1 className='font-serif text-7xl font-bold'>Hi, I'm Alexis</h1>
+        <h2 className='text-grey-text max-w-lg text-lg'>
+          Front-end web wizard who enjoys creating a variety of tools and a
+          continuous dedication to learning and self-development.
+        </h2>
+      </div>
     </div>
   );
 };
