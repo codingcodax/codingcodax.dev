a9db79b71c06fe2551e6ba7225d662d2a24b576c
Alexis Guzman <codingcodax@gmail.com>
2024-07-16 18:03:21 -0600

    feat(pages): add hero component

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
 
+import { Hero } from './_components/Hero';
+
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>home page</p>
+    <div className='min-h-screen'>
+      <Hero />
     </div>
   );
 };
