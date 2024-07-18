e1d988e9d592f70f949809c76eaabf8ac9fbec04
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 10:24:56 -0600

    feat(components): setup project title

--- apps/www/src/app/_components/Project/ProjectTitle.tsx
+++ apps/www/src/app/_components/Project/ProjectTitle.tsx
@@ -1,7 +1,5 @@
-export const ProjectTitle = () => {
-  return (
-    <div>
-      <p>project title</p>
-    </div>
-  );
+type Props = { children: React.ReactNode };
+
+export const ProjectTitle = ({ children }: Props) => {
+  return <h4 className='font-serif text-xl'>{children}</h4>;
 };
