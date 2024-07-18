0b959723195b9d4936d774b1d94914c795e1494b
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 12:15:05 -0600

    feat(components): adjust styling
    -  Make it full width to simulate the rounded top page

--- apps/www/src/app/(main)/_components/Header/Header.tsx
+++ apps/www/src/app/(main)/_components/Header/Header.tsx
@@ -2,14 +2,18 @@ import { Navbar } from './Navbar';
 
 export const Header = () => {
   return (
-    <div className='sticky top-px z-10 mx-auto flex w-full max-w-screen-lg items-center justify-center bg-grey-base py-4'>
-      <Navbar />
+    <div className='sticky top-0 z-10 -mt-[25px] bg-white pt-[24px] dark:bg-black'>
+      <div className='-ml-[0.9px] w-[calc(100%+2px)] rounded-t-3xl border border-grey-line bg-grey-base'>
+        <div className='relative mx-auto flex w-full max-w-screen-lg items-center justify-center py-4'>
+          <Navbar />
 
-      {/* borders */}
-      <div className='absolute -top-px left-0 h-px w-full bg-grey-line' />
-      <div className='absolute -left-px top-0 h-full w-px bg-grey-line' />
-      <div className='absolute -right-px top-0 h-full w-px bg-grey-line' />
-      <div className='absolute -bottom-px left-0 h-px w-full bg-grey-line' />
+          <div className='absolute -left-px top-0 h-full w-px bg-grey-line' />
+          <div className='absolute -right-px top-0 h-full w-px bg-grey-line' />
+        </div>
+      </div>
+
+      <div className='absolute -left-px top-0 h-[43px] w-px bg-white dark:bg-black' />
+      <div className='absolute -right-px top-0 h-[43px] w-px bg-white dark:bg-black' />
     </div>
   );
 };
