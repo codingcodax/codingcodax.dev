61917c311e52ee3abe34f3a1408870fb4bf12f08
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 11:37:29 -0600

    feat(layout): reset

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -7,7 +7,6 @@ import { cn } from '@acme/ui';
 import '~/app/globals.css';
 
 import { env } from '~/env';
-import { Header } from './_components/Header';
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
@@ -50,22 +49,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           GeistMono.variable,
         )}
       >
-        <div className='relative flex flex-col rounded-3xl border border-grey-line bg-grey-base'>
-          <Providers>
-            <div className='absolute left-0 top-[68px] h-px w-full bg-grey-line' />
-
-            <Header />
-            <div className='relative mx-auto w-full max-w-screen-lg pb-[68px]'>
-              <div className='absolute -left-px top-0 h-full w-px bg-grey-line' />
-              <div className='absolute left-1/3 top-0 h-full w-px border border-dashed border-grey-bg-hover' />
-              {props.children}
-              <div className='absolute right-1/3 top-0 h-full w-px border border-dashed border-grey-bg-hover' />
-              <div className='absolute -right-px top-0 h-full w-px bg-grey-line' />
-            </div>
-
-            <div className='absolute bottom-[68px] left-0 h-px w-full bg-grey-line' />
-          </Providers>
-        </div>
+        <Providers>{props.children}</Providers>
       </body>
     </html>
   );
