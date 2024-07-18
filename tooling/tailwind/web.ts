208a91896e22968845a7aa50ed2f4a1ee0823fd5
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 17:20:35 -0600

    feat(tailwindcss): add `logo-cloud` and `logo-cloud-revers` animations

--- tooling/tailwind/web.ts
+++ tooling/tailwind/web.ts
@@ -29,10 +29,20 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'logo-cloud': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(calc(-100% - 4rem))' },
+        },
+        'logo-cloud-reverse': {
+          from: { transform: 'translateX(calc(-100% - 4rem))' },
+          to: { transform: 'translateX(0)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'logo-cloud': 'logo-cloud 30s linear infinite',
+        'logo-cloud-reverse': 'logo-cloud-reverse 30s linear infinite',
       },
     },
   },
