8060f32bcd9f4e6b40bebfda914e67270d626dba
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 10:24:47 -0600

    feat(components): setup project image

--- apps/www/src/app/_components/Project/ProjectImage.tsx
+++ apps/www/src/app/_components/Project/ProjectImage.tsx
@@ -1,7 +1,9 @@
 export const ProjectImage = () => {
   return (
-    <div>
-      <p>project image</p>
+    <div className='col-span-2'>
+      <figure className='relative z-[1] scale-90 shadow shadow-black-a1'>
+        <div className='aspect-video h-full w-full bg-grey-bg' />
+      </figure>
     </div>
   );
 };
