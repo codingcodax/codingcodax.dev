28fd3603c835cfb0a544bf8005bc6d693338abbb
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 17:49:15 -0600

    feat(components): setup coding hours

--- apps/www/src/app/(main)/_components/CodingHours/CodingHours.tsx
+++ apps/www/src/app/(main)/_components/CodingHours/CodingHours.tsx
@@ -1,7 +1,30 @@
-export const CodingHours = () => {
+import { env } from '~/env';
+
+export const CodingHours = async () => {
+  const res = await fetch(
+    'https://wakatime.com/api/v1/users/current/all_time_since_today',
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(env.WAKATIME_API_KEY).toString('base64')}`,
+      },
+    },
+  );
+
+  const {
+    data: { total_seconds },
+  } = await res.json();
+
+  const roundedSeconds = Math.round(total_seconds);
+  const hours = Math.floor(roundedSeconds / 3600).toLocaleString();
+  const remainingSeconds = roundedSeconds % 3600;
+  const minutes = Math.floor(remainingSeconds / 60);
+
   return (
-    <div>
-      <p>coding hours</p>
+    <div className='space-y-2 px-8'>
+      <h4 className='font-serif text-lg text-grey-text'>Coding hours</h4>
+      <p className='text-3xl font-medium'>
+        {hours} hrs {minutes} mins
+      </p>
     </div>
   );
 };
