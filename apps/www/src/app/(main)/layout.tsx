b9bba3192303d5de59f62fbade2561e520ca1824
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 11:37:41 -0600

    feat(layout): add lines

--- apps/www/src/app/(main)/layout.tsx
+++ apps/www/src/app/(main)/layout.tsx
@@ -1,7 +1,21 @@
 type Props = { children: React.ReactNode };
 
 const Layout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <div className='relative flex flex-col rounded-3xl border border-grey-line bg-grey-base'>
+      <div className='absolute left-0 top-[68px] h-px w-full bg-grey-line' />
+
+      <div className='relative mx-auto w-full max-w-screen-lg pb-[68px]'>
+        <div className='absolute -left-px top-0 h-full w-px bg-grey-line' />
+        <div className='absolute left-1/3 top-0 h-full w-px border border-dashed border-grey-bg-hover' />
+        {children}
+        <div className='absolute right-1/3 top-0 h-full w-px border border-dashed border-grey-bg-hover' />
+        <div className='absolute -right-px top-0 h-full w-px bg-grey-line' />
+      </div>
+
+      <div className='absolute bottom-[68px] left-0 h-px w-full bg-grey-line' />
+    </div>
+  );
 };
 
 export default Layout;
