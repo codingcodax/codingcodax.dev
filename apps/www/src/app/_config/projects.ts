fdf144ed9a82d099e6c1deda08f429d5d5764899
Alexis Guzman <codingcodax@gmail.com>
2024-07-18 10:26:56 -0600

    feat(config): create projects array

---
+++ apps/www/src/app/_config/projects.ts
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    name: 'TIQR',
+    description:
+      'A comprehensive platform for purchasing tickets to concerts, festivals, and events, offering a seamless and secure online experience.',
+    imageUrl: '',
+    alt: '',
+  },
+  {
+    name: 'Creeaty',
+    description:
+      'A creator-centric platform facilitating partnerships between hotels and a diverse array of genuine content creators worldwide.',
+    imageUrl: '',
+    alt: '',
+  },
+  {
+    name: 'Nomada Home',
+    description:
+      'A platform connecting travelers and renters with property owners, offering flexible monthly rentals for extended stays.',
+    imageUrl: '',
+    alt: '',
+  },
+];
