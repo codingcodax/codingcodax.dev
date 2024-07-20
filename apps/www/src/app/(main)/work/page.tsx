485234f6a5f369d58b8ca20e6be33eb975bd1674
Alexis Guzman <codingcodax@gmail.com>
2024-07-20 13:00:05 -0600

    feat(pages): add projects

--- apps/www/src/app/(main)/work/page.tsx
+++ apps/www/src/app/(main)/work/page.tsx
@@ -1,11 +1,30 @@
 import type { NextPage } from 'next';
 
-import { Hero } from '../_components/Hero';
+import { Hero } from '~/app/(main)/_components/Hero';
+import {
+  Project,
+  ProjectContent,
+  ProjectDescription,
+  ProjectImage,
+  ProjectTitle,
+} from '~/app/(main)/_components/Project';
+import { projects } from '~/config/projects';
 
 const Work: NextPage = () => {
   return (
     <main>
       <Hero>Work</Hero>
+
+      {projects.map(({ name, description }) => (
+        <Project key={name}>
+          <ProjectContent>
+            <ProjectTitle>{name}</ProjectTitle>
+            <ProjectDescription>{description}</ProjectDescription>
+          </ProjectContent>
+
+          <ProjectImage />
+        </Project>
+      ))}
     </main>
   );
 };
