4d46ea0afae6dacea6ac749de37ebc6383a44590
Alexis Guzman <codingcodax@gmail.com>
2024-07-20 10:33:35 -0600

    feat(components): setup contact me

--- apps/www/src/app/(main)/_components/ContactMe/ContactMe.tsx
+++ apps/www/src/app/(main)/_components/ContactMe/ContactMe.tsx
@@ -1,7 +1,46 @@
-export const ContactMe = () => {
+import { clsx } from 'clsx';
+
+type Props = { variant: 'expanded' | 'collapsed' };
+
+export const ContactMe = ({ variant }: Props) => {
   return (
-    <div>
-      <p>contact me</p>
-    </div>
+    <section
+      className={clsx('relative p-8', variant === 'expanded' ? 'h-[40vh]' : '')}
+    >
+      <div className='absolute -top-px left-0 h-px w-full bg-grey-line' />
+
+      <div className='gap flex h-full flex-col justify-center border-2 border-dotted border-grey-line bg-grey-base p-8'>
+        <h4 className='font-serif text-3xl'>Let’s work together</h4>
+        <p className='text-grey-text'>
+          If you have any questions, opportunities or would just like to say
+          hello, then feel free to send me a DM (
+          <a
+            className='text-grey-text-contrast decoration-grey-text-contrast decoration-dashed underline-offset-2 hover:underline'
+            href='https://dm.new/codingcodax'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            X
+          </a>{' '}
+          or{' '}
+          <a
+            className='text-grey-text-contrast decoration-grey-text-contrast decoration-dashed underline-offset-2 hover:underline'
+            href='https://www.linkedin.com/in/codingcodax'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            LinkedIn
+          </a>
+          ) or if you prefer, you can{' '}
+          <a
+            className='text-grey-text-contrast decoration-grey-text-contrast decoration-dashed underline-offset-2 hover:underline'
+            href='emailto:codingcodax@gmail.com'
+          >
+            email me
+          </a>
+          .
+        </p>
+      </div>
+    </section>
   );
 };
