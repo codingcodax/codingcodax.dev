f86cf059f3dddc30bd092e4a12da8411de9ac44b
Alexis Guzman <codingcodax@gmail.com>
2024-07-20 12:57:05 -0600

    feat(components): remove bottom line

--- apps/www/src/app/(main)/_components/Intro/Intro.tsx
+++ apps/www/src/app/(main)/_components/Intro/Intro.tsx
@@ -5,15 +5,13 @@ export const Intro = () => {
     <div className='relative flex min-h-[50vh] flex-col items-center justify-center bg-grey-base px-2 pb-4 pt-8'>
       <IntroImage />
 
-      <div className='mt-6 flex flex-col items-center justify-center text-center'>
-        <h1 className='font-serif text-[80px] font-bold'>Hi, I'm Alexis</h1>
+      <main className='mt-6 flex flex-col items-center justify-center text-center'>
+        <h1 className='font-serif text-6xl font-bold'>Hi, I'm Alexis</h1>
         <h2 className='max-w-lg text-lg text-grey-text'>
           Front-end web wizard who enjoys creating a variety of tools and a
           continuous dedication to learning and self-development.
         </h2>
-      </div>
-
-      <div className='absolute -bottom-px right-0 h-px w-full bg-grey-line' />
+      </main>
     </div>
   );
 };
