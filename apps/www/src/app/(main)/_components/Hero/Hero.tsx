568e0e490aa9f86b8d2f976ab05aba48c5b1d8cf
Alexis Guzman <codingcodax@gmail.com>
2024-07-20 12:59:49 -0600

    feat(components): add bottom line

--- apps/www/src/app/(main)/_components/Hero/Hero.tsx
+++ apps/www/src/app/(main)/_components/Hero/Hero.tsx
@@ -6,6 +6,8 @@ export const Hero = ({ children }: Props) => {
       <div className='border border-grey-line px-8 py-4'>
         <h1 className='font-serif text-6xl font-bold'>{children}</h1>
       </div>
+
+      <div className='absolute -bottom-px left-0 h-px w-full bg-grey-line' />
     </div>
   );
 };
