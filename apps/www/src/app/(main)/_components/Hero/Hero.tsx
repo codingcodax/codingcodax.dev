704923afd366438fba34e2005de12b04b140ea41
Alexis Guzman <codingcodax@gmail.com>
2024-07-20 13:08:13 -0600

    feat(components): add background color

--- apps/www/src/app/(main)/_components/Hero/Hero.tsx
+++ apps/www/src/app/(main)/_components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ type Props = { children: React.ReactNode };
 
 export const Hero = ({ children }: Props) => {
   return (
-    <div className='relative flex min-h-[50vh] items-center justify-center'>
+    <div className='relative flex min-h-[50vh] items-center justify-center bg-grey-base'>
       <div className='border border-grey-line px-8 py-4'>
         <h1 className='font-serif text-6xl font-bold'>{children}</h1>
       </div>
