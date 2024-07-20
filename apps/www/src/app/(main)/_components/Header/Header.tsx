4e49d169dd4912f94d0690a1f4d42e01f84a185b
Alexis Guzman <codingcodax@gmail.com>
2024-07-20 11:01:43 -0600

    feat(components): replace `div` with `header` tag

--- apps/www/src/app/(main)/_components/Header/Header.tsx
+++ apps/www/src/app/(main)/_components/Header/Header.tsx
@@ -4,12 +4,12 @@ export const Header = () => {
   return (
     <div className='sticky top-0 z-10 -mt-[25px] bg-white pt-[24px] dark:bg-black'>
       <div className='-ml-[0.9px] w-[calc(100%+2px)] rounded-t-3xl border border-grey-line bg-grey-base'>
-        <div className='relative mx-auto flex w-full max-w-screen-lg items-center justify-center py-4'>
+        <header className='relative mx-auto flex w-full max-w-screen-lg items-center justify-center py-4'>
           <Navbar />
 
           <div className='absolute -left-px top-0 h-full w-px bg-grey-line' />
           <div className='absolute -right-px top-0 h-full w-px bg-grey-line' />
-        </div>
+        </header>
       </div>
 
       <div className='absolute -left-px top-0 h-[43px] w-px bg-white dark:bg-black' />
