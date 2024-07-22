779d5ae6475c82e50296c5f858170e42e5cda0d4
Alexis Guzman <codingcodax@gmail.com>
2024-07-22 16:22:07 -0600

    feat(components): setup block title

--- apps/www/src/app/(main)/about/_components/Block/BlockTitle.tsx
+++ apps/www/src/app/(main)/about/_components/Block/BlockTitle.tsx
@@ -1,7 +1,5 @@
-export const BlockTitle = () => {
-  return (
-    <div>
-      <p>block title</p>
-    </div>
-  );
+type Props = { children: React.ReactNode };
+
+export const BlockTitle = ({ children }: Props) => {
+  return <h2>{children}</h2>;
 };
