1db9c336f615e533f0e8a799055e20f19540fd70
Alexis Guzman <codingcodax@gmail.com>
2024-07-22 16:24:19 -0600

    feat(components): add bottom line

--- apps/www/src/app/(main)/about/_components/Block/Block.tsx
+++ apps/www/src/app/(main)/about/_components/Block/Block.tsx
@@ -1,5 +1,11 @@
 type Props = { children: React.ReactNode };
 
 export const Block = ({ children }: Props) => {
-  return <div className='grid grid-cols-3'>{children}</div>;
+  return (
+    <div className='relative grid grid-cols-3'>
+      {children}
+
+      <div className='absolute -bottom-px left-0 h-px w-full bg-grey-line' />
+    </div>
+  );
 };
