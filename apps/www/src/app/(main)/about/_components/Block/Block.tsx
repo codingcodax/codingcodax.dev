4fb303e8dd7d8cab127087b68a842faaf1279787
Alexis Guzman <codingcodax@gmail.com>
2024-07-22 16:21:56 -0600

    feat(components): setup block

--- apps/www/src/app/(main)/about/_components/Block/Block.tsx
+++ apps/www/src/app/(main)/about/_components/Block/Block.tsx
@@ -1,7 +1,5 @@
-export const Block = () => {
-  return (
-    <div>
-      <p>block</p>
-    </div>
-  );
+type Props = { children: React.ReactNode };
+
+export const Block = ({ children }: Props) => {
+  return <div className='grid grid-cols-3'>{children}</div>;
 };
