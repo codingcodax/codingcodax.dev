4a778f3cca2c76e75caf5042477bc6fc056af8cf
Alexis Guzman <codingcodax@gmail.com>
2024-07-22 16:22:01 -0600

    feat(components): setup block content

--- apps/www/src/app/(main)/about/_components/Block/BlockContent.tsx
+++ apps/www/src/app/(main)/about/_components/Block/BlockContent.tsx
@@ -1,7 +1,5 @@
-export const BlockContent = () => {
-  return (
-    <div>
-      <p>block content</p>
-    </div>
-  );
+type Props = { children: React.ReactNode };
+
+export const BlockContent = ({ children }: Props) => {
+  return <div className='col-span-2 grid-cols-subgrid p-px'>{children}</div>;
 };
