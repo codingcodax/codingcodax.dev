1ff2e62437ebc45404b21b8e73aa49c4f13056a9
Alexis Guzman <codingcodax@gmail.com>
2024-07-22 18:47:07 -0600

    feat(pages): add work experience and education blocks

--- apps/www/src/app/(main)/about/page.tsx
+++ apps/www/src/app/(main)/about/page.tsx
@@ -2,6 +2,13 @@ import type { NextPage } from 'next';
 
 import { Hero } from '~/app/(main)/_components/Hero';
 import { Block, BlockContent, BlockTitle } from './_components/Block';
+import { DateLabel } from './_components/DateLabel';
+import {
+  DetailBlock,
+  DetailBlockContent,
+  DetailBlockSubtitle,
+  DetailBlockTitle,
+} from './_components/DetailBlock';
 
 const About: NextPage = () => {
   return (
@@ -53,6 +60,72 @@ const About: NextPage = () => {
               learn from others and collaborate on exciting projects that make a
               difference.
             </p>
+          </div>
+        </BlockContent>
+      </Block>
+
+      <Block>
+        <BlockTitle>Work Experience</BlockTitle>
+
+        <BlockContent>
+          <div className='relative grid grid-cols-2'>
+            <DateLabel>JUL - NOV 2023</DateLabel>
+            <DetailBlock>
+              <DetailBlockTitle>Co-Founder</DetailBlockTitle>
+              <DetailBlockSubtitle>Docom - Healthcare CRM</DetailBlockSubtitle>
+
+              <DetailBlockContent>
+                <p className='text-sm text-grey-text'>
+                  Led a talented team in developing a healthcare CRM aimed at
+                  enhancing patient management and communication. Although the
+                  project didn’t reach completion, it provided invaluable
+                  insights into product development and the healthcare
+                  landscape.
+                </p>
+              </DetailBlockContent>
+            </DetailBlock>
+          </div>
+
+          <div className='w-full border border-dashed border-grey-line' />
+
+          <div className='grid grid-cols-2'>
+            <DateLabel>JUN 2023 - NOW</DateLabel>
+            <DetailBlock>
+              <DetailBlockTitle>Co-Founder</DetailBlockTitle>
+              <DetailBlockSubtitle>Khutz - Digital Agency</DetailBlockSubtitle>
+
+              <DetailBlockContent>
+                <p className='text-sm text-grey-text'>
+                  I collaborate closely with clients to create tailored
+                  solutions that not only meet their needs but also elevate
+                  their brand presence online. My focus is on blending
+                  functionality with creativity to deliver standout digital
+                  experiences.
+                </p>
+              </DetailBlockContent>
+            </DetailBlock>
+          </div>
+        </BlockContent>
+      </Block>
+
+      <Block>
+        <BlockTitle>Education</BlockTitle>
+
+        <BlockContent>
+          <div className='grid grid-cols-2'>
+            <DateLabel>DEC 2019 - JAN 2021</DateLabel>
+            <DetailBlock>
+              <DetailBlockTitle>Full Stack Developer</DetailBlockTitle>
+              <DetailBlockSubtitle>Bottega - Bootcamp</DetailBlockSubtitle>
+
+              <DetailBlockContent>
+                <p className='text-sm text-grey-text'>
+                  I developed a strong foundation in web technologies, including
+                  HTML, CSS, JavaScript, Python, and React. I acquired the tools
+                  to craft innovative solutions and bring ideas to life.
+                </p>
+              </DetailBlockContent>
+            </DetailBlock>
           </div>
         </BlockContent>
       </Block>
