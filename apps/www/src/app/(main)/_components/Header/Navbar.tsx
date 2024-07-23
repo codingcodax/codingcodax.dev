774da9d85a9ab2e84ee105920a2e279c32934acb
Alexis Guzman <codingcodax@gmail.com>
2024-07-23 12:51:49 -0600

    feat(components): add `z-index`

--- apps/www/src/app/(main)/_components/Header/Navbar.tsx
+++ apps/www/src/app/(main)/_components/Header/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavItem } from './NavItem';
 
 export const Navbar = () => {
   return (
-    <div className='flex gap-2'>
+    <div className='z-[1] flex gap-2'>
       <NavItem href='/'>Home</NavItem>
       <NavItem href='/work'>Work</NavItem>
       <NavItem href='/about'>About</NavItem>
