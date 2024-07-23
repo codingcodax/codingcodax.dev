3e9b585ae31c7d7d05e799409e6874b1ad298a59
Alexis Guzman <codingcodax@gmail.com>
2024-07-23 12:51:40 -0600

    feat(components): add border bottom

--- apps/www/src/app/(main)/about/_components/Block/BlockContent.tsx
+++ apps/www/src/app/(main)/about/_components/Block/BlockContent.tsx
@@ -1,5 +1,9 @@
 type Props = { children: React.ReactNode };
 
 export const BlockContent = ({ children }: Props) => {
-  return <div className='col-span-2 grid-cols-subgrid p-px'>{children}</div>;
+  return (
+    <div className='col-span-2 grid-cols-subgrid divide-y divide-dashed divide-grey-line pl-px'>
+      {children}
+    </div>
+  );
 };
