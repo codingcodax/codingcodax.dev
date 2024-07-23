eb54d849d72ff29b7ea572ca6a2c99968b3b9566
Alexis Guzman <codingcodax@gmail.com>
2024-07-23 09:11:59 -0600

    feat(components): add bottom line

--- apps/www/src/app/(main)/_components/Intro/Intro.tsx
+++ apps/www/src/app/(main)/_components/Intro/Intro.tsx
@@ -12,6 +12,8 @@ export const Intro = () => {
           continuous dedication to learning and self-development.
         </h2>
       </main>
+
+      <div className='absolute -bottom-px left-0 h-px w-full bg-grey-line' />
     </div>
   );
 };
