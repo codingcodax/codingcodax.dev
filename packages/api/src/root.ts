0aa4be0a7032303d4aa3e1cc1f586b14def21782
Alexis Guzman <codingcodax@gmail.com>
2024-07-24 10:34:10 -0600

    feat(api): add wakatime router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
 import { helloRouter } from './router/hello';
+import { wakatimeRouter } from './router/wakatime';
 import { createTRPCRouter } from './trpc';
 
 export const appRouter = createTRPCRouter({
   hello: helloRouter,
+  wakatime: wakatimeRouter,
 });
 
 // export type definition of API
