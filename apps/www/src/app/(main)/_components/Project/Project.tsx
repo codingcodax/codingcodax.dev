103dbb893955ba40d63ca6278deddda01fd2bb0a
Alexis Guzman <codingcodax@gmail.com>
2024-07-24 18:11:58 -0600

    feat(components): add `group` utility classname

--- apps/www/src/app/(main)/_components/Project/Project.tsx
+++ apps/www/src/app/(main)/_components/Project/Project.tsx
@@ -2,7 +2,7 @@ type Props = { children: React.ReactNode };
 
 export const Project = ({ children }: Props) => {
   return (
-    <div className='relative grid grid-cols-3 border-b border-grey-line'>
+    <div className='group relative grid grid-cols-3 border-b border-grey-line'>
       {children}
     </div>
   );
