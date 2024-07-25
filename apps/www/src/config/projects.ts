8815fd2619f6b65295013d75a0a51771efcc09f3
Alexis Guzman <codingcodax@gmail.com>
2024-07-24 18:16:51 -0600

    feat(config): add image and alt props to `projects` and `sideProjects`

--- apps/www/src/config/projects.ts
+++ apps/www/src/config/projects.ts
@@ -1,24 +1,30 @@
+import CreeatyPreview from '../../public/images/creeaty-preview.png';
+import DDGThemesPreview from '../../public/images/ddg-themes-preview.png';
+import MiniPreview from '../../public/images/mini-preview.png';
+import NomadaHomePreview from '../../public/images/nomada-home-preview.png';
+import TIQRPreview from '../../public/images/tiqr-preview.png';
+
 export const projects = [
   {
     name: 'TIQR',
     description:
       'A comprehensive platform for purchasing tickets to concerts, festivals, and events, offering a seamless and secure online experience.',
-    imageUrl: '',
-    alt: '',
+    image: TIQRPreview,
+    alt: 'Preview of the TIQR web app',
   },
   {
     name: 'Creeaty',
     description:
       'A creator-centric platform facilitating partnerships between hotels and a diverse array of genuine content creators worldwide.',
-    imageUrl: '',
-    alt: '',
+    image: CreeatyPreview,
+    alt: 'Preview of the Creeaty web app',
   },
   {
     name: 'Nomada Home',
     description:
       'A platform connecting travelers and renters with property owners, offering flexible monthly rentals for extended stays.',
-    imageUrl: '',
-    alt: '',
+    image: NomadaHomePreview,
+    alt: 'Preview of the Nomada Home web app',
   },
 ];
 
@@ -27,10 +33,14 @@ export const sideProjects = [
     name: 'mini',
     description:
       'A versatile design system offering a curated library of TailwindCSS-built UI components for intuitive interfaces.',
+    image: MiniPreview,
+    alt: 'Preview of the mini web app',
   },
   {
     name: 'DDG Themes',
     description:
       'Enhance your DuckDuckGo experience with a diverse collection of customizable themes, effortlessly installable for a personalized search.',
+    image: DDGThemesPreview,
+    alt: 'Preview of the DDG Themes web app',
   },
 ];
