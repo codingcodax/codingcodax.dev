6bfbb3efc0482bd5cec31e97914ff0ef3ecf5a6a
Alexis Guzman <codingcodax@gmail.com>
2024-07-24 18:12:21 -0600

    feat(components): scale to 100 with `group-hover:`

--- apps/www/src/app/(main)/_components/Project/ProjectImage.tsx
+++ apps/www/src/app/(main)/_components/Project/ProjectImage.tsx
@@ -1,12 +1,20 @@
+import type { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import { clsx } from 'clsx/lite';
 
 import { fadeUp } from '~/config/animations';
 
-export const ProjectImage = () => {
+type Props = { image: StaticImageData; alt: string };
+
+export const ProjectImage = ({ image, alt }: Props) => {
   return (
-    <div className={clsx('col-span-2', fadeUp)}>
-      <figure className='relative z-[1] scale-90 shadow shadow-black-a1'>
-        <div className='aspect-video h-full w-full bg-grey-bg' />
+    <div className={clsx('col-span-2 pl-px', fadeUp)}>
+      <figure className='relative z-[1] scale-90 grayscale transition-transform group-hover:scale-100'>
+        <Image
+          alt={alt}
+          className='aspect-video h-full w-full bg-grey-bg'
+          src={image}
+        />
       </figure>
     </div>
   );
