84800b9640380277cd939da0e748e89d8088e1fe
Alexis Guzman <codingcodax@gmail.com>
2024-07-31 13:23:12 -0600

    feat(components): add flick up styles

--- apps/www/src/app/(main)/_components/Header/NavItem.tsx
+++ apps/www/src/app/(main)/_components/Header/NavItem.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { clsx } from 'clsx/lite';
 
 import { Button } from '@acme/ui/Button';
 
@@ -18,10 +19,18 @@ export const NavItem = ({ className, href, children }: Props) => {
   return (
     <Button
       asChild
-      className={className}
+      className={clsx('group relative overflow-hidden', className)}
       variant={isActive ? 'solid' : 'ghost'}
     >
-      <Link href={href}>{children}</Link>
+      <Link href={href}>
+        <span className='invisible'>{children}</span>
+        <span className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-500 ease-in-out hover:duration-300 group-hover:-translate-y-[200%]'>
+          {children}
+        </span>
+        <span className='absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-[200%] transition-transform duration-500 ease-in-out hover:duration-300 group-hover:-translate-y-1/2'>
+          {children}
+        </span>
+      </Link>
     </Button>
   );
 };
