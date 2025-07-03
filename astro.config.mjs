a18e4bc3d3e1a09c6f7f7153d7b8f9e44205a267
Alexis Guzman <codingcodax@gmail.com>
2025-07-02 20:52:30 -0600

    style(prettier): run `format:fix` script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
