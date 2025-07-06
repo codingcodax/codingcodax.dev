f5351e63b2f21deb8ab3fb16e89ff58967b3445e
Alexis Guzman <codingcodax@gmail.com>
2025-07-06 14:32:21 -0600

    feat(config): create `technologies` array

---
+++ src/config/technologies.ts
@@ -0,0 +1,37 @@
+import CSSIcon from '~/components/icons/css.astro';
+import FigmaIcon from '~/components/icons/figma.astro';
+import GitIcon from '~/components/icons/git.astro';
+import GitHubIcon from '~/components/icons/github.astro';
+import HTMLIcon from '~/components/icons/html.astro';
+import JavaScriptIcon from '~/components/icons/javascript.astro';
+import MySQLIcon from '~/components/icons/mysql.astro';
+import NextJSIcon from '~/components/icons/nextjs.astro';
+import NodeJSIcon from '~/components/icons/nodejs.astro';
+import NPMIcon from '~/components/icons/npm.astro';
+import PrismaIcon from '~/components/icons/prisma.astro';
+import RadixUIIcon from '~/components/icons/radix.astro';
+import ReactIcon from '~/components/icons/react.astro';
+import TailwindCSSIcon from '~/components/icons/tailwindcss.astro';
+import TRPCIcon from '~/components/icons/trpc.astro';
+import TypeScriptIcon from '~/components/icons/typescript.astro';
+import ZodIcon from '~/components/icons/zod.astro';
+
+export const technologies = [
+  { name: 'CSS', icon: CSSIcon },
+  { name: 'Figma', icon: FigmaIcon },
+  { name: 'Git', icon: GitIcon },
+  { name: 'GitHub', icon: GitHubIcon },
+  { name: 'HTML', icon: HTMLIcon },
+  { name: 'JavaScript', icon: JavaScriptIcon },
+  { name: 'MySQL', icon: MySQLIcon },
+  { name: 'Next JS', icon: NextJSIcon },
+  { name: 'Node JS', icon: NodeJSIcon },
+  { name: 'NPM', icon: NPMIcon },
+  { name: 'Prisma', icon: PrismaIcon },
+  { name: 'Radix UI', icon: RadixUIIcon },
+  { name: 'React JS', icon: ReactIcon },
+  { name: 'Tailwind CSS', icon: TailwindCSSIcon },
+  { name: 'tRPC', icon: TRPCIcon },
+  { name: 'TypeScript', icon: TypeScriptIcon },
+  { name: 'Zod', icon: ZodIcon },
+];
