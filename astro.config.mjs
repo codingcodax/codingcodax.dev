845c766447d714ff5450fe9cedaf8abdd5f2f9d9
Alexis Guzman <codingcodax@gmail.com>
2025-07-06 14:33:00 -0600

    feat(astro): add `site`

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.codingcodax.dev',
   integrations: [react(), sitemap()],
 
   vite: {
