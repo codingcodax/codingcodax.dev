290e9de895d5e3c09703226c30381a3b34d2b80e
Alexis Guzman <codingcodax@gmail.com>
2025-07-12 20:43:33 -0600

    feat(astro): add i18n config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/config/locales';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.codingcodax.dev',
+  base: '/',
   integrations: [react(), sitemap()],
 
   vite: {
@@ -21,5 +24,14 @@ export default defineConfig({
         pathname: '/u/**',
       },
     ],
+  },
+
+  i18n: {
+    defaultLocale: DEFAULT_LOCALE_SETTING,
+    locales: Object.keys(LOCALES_SETTING),
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
   },
 });
