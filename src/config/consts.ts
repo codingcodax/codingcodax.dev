ec3d1b632a0245707eb6bdd5b72b8fb37f5ed03c
Alexis Guzman <codingcodax@gmail.com>
2025-07-12 20:40:12 -0600

    feat(config): add `en` and `es`

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,3 +1,5 @@
+import type { Multilingual } from '~/i18n';
+
 export const SITE = {
   NAME: 'Alexis Guzman',
   EMAIL: 'codingcodax@gmail.com',
@@ -10,14 +12,44 @@ export const HOME = {
     'Front-end web wizard who enjoys creating a variety of tools and a continuous dedication to learning and self-development.',
 };
 
+export const HOME_TITLE: Multilingual = {
+  en: 'Home',
+  es: 'Inicio',
+};
+
+export const HOME_DESCRIPTION: Multilingual = {
+  en: 'Front-end web wizard who enjoys creating a variety of tools and a continuous dedication to learning and self-development.',
+  es: 'Front-end web wizard que disfruta creando diversas herramientas y con una dedicación continua a aprender y autodesarrollarse.',
+};
+
 export const WORK = {
   TITLE: 'Work',
   DESCRIPTION: 'See all the projects I have worked on.',
 };
 
+export const WORK_TITLE: Multilingual = {
+  en: 'Work',
+  es: 'Mi trabajo',
+};
+
+export const WORK_DESCRIPTION: Multilingual = {
+  en: 'See all the projects I have worked on.',
+  es: 'Ve todos los proyectos en los que he trabajado.',
+};
+
 export const ABOUT = {
   TITLE: 'About',
   DESCRIPTION: 'Learn more about me and my journey as a developer.',
+};
+
+export const ABOUT_TITLE: Multilingual = {
+  en: 'About',
+  es: 'Sobre mí',
+};
+
+export const ABOUT_DESCRIPTION: Multilingual = {
+  en: 'Learn more about me and my journey as a developer.',
+  es: 'Aprende más sobre mí y mi viaje como desarrollador.',
 };
 
 export const SOCIALS = [
